18987bd75bc051c6bc4ae526d28691bebe5a68ed
yannikschaelte <yannik.schaelte@gmail.com>
2018-07-30 10:12:41 +0200

    init setup.py, sphinx

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "pypesto", "version.py")) as f:
+    version = f.read().split('\n')[0].split('=')[-1].strip(' ').strip('"')
+
+setup(name='pypesto',
+      version=version,
+      description="Parameter EStimation TOolbox",
+      author="The pyPESTO developers",
+      author_email="yannik.schaelte@gmail.com",
+      url="https://github.com/icb-dcm/pyPESTO",
+      packages=find_packages(exclude=["example*", "test*"])
+     )
