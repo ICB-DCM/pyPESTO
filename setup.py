bbe2c5a9191e9776cd93b73529834a040629921b
yannikschaelte <yannik.schaelte@gmail.com>
2018-08-19 00:08:39 +0200

    improve setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ import os
 with open(os.path.join(os.path.dirname(__file__), "pesto", "version.py")) as f:
     version = f.read().split('\n')[0].split('=')[-1].strip(' ').strip('"')
 
-setup(name='pesto',
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='pypesto',
       version=version,
       description="Parameter EStimation TOolbox",
+      long_description=read('README.md'),
       author="The PESTO developers",
       author_email="yannik.schaelte@gmail.com",
       url="https://github.com/icb-dcm/pypesto",
-      packages=find_packages(exclude=["example*", "test*"]),
-      install_requires=['numpy', 'scipy', 'matplotlib']
+      packages=find_packages(exclude=["example*", "test*"])
       )
