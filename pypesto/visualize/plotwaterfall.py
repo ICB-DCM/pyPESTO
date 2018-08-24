5febcac66c40b0a00cc734d9272c60ed0221b00f
Dantong Wang <dantong.wang@helmholtz-muenchen.de>
2018-08-24 22:46:02 +0200

    add plot waterfall

---
+++ pypesto/visualize/plotwaterfall.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+from scipy import cluster
+
+def plotwaterfall(result):
+    """
+
+    Plot waterwall plot.
+
+    Parameters
+    ----------
+
+    result: pypesto.Result
+        Optimization result obtained by 'optimize.py'
+
+    """
+
+    # get values of cost function from result
+    result_fval = result.optimize_result.get_for_key('fval')
+    result_fval = np.reshape(result_fval,[len(result_fval), 1])
+    startpoints = range(1, len(result_fval) + 1)
+
+    # clustering
+    clust = cluster.hierarchy.fcluster(cluster.hierarchy.linkage(result_fval), 0.1, criterion='distance')
+    uclust, ind_clust = np.unique(clust, return_inverse=True)
+    clustsize = np.zeros(len(uclust))
+    for iclustsize in range(len(uclust)):
+        clustsize[iclustsize] = sum(clust == uclust[iclustsize])
+
+    # assign colors
+    jet = plt.get_cmap('jet')
+    vmax = len(uclust)-sum(clustsize == 1)
+    cNorm  = colors.Normalize(vmin=0, vmax=vmax)
+    scalarMap = cm.ScalarMappable(norm=cNorm, cmap=jet)
+
+    # plot
+    ax = plt.figure().gca()
+    ax.plot(startpoints, result_fval)
+    sum_grey = 1
+    for iind in range(len(ind_clust)):
+        if clustsize[ind_clust[iind]] == 1:
+            ax.plot(startpoints[iind], result_fval[iind], color='lightgrey', marker='o')
+            sum_grey = sum_grey
+        else:
+            Col = scalarMap.to_rgba(uclust[ind_clust[iind]]-sum_grey)
+            ax.plot(startpoints[iind], result_fval[iind], color=Col, marker='o')
+
+    if clustsize[ind_clust[0]] > 1:
+        Col0 = (1, 0, 0, 1)
+        ax.plot(1, result_fval[0], color=Col0, marker='o')
+
+    plt.show()
