ad65044ed21455a7a5eba881a42e2970fc6cf791
yannikschaelte <yannik.schaelte@gmail.com>
2018-08-31 13:36:59 +0200

    test_visualize to unittest

--- test/test_visualize.py
+++ test/test_visualize.py
@@ -2,6 +2,7 @@ import pypesto
 import pypesto.visualize
 import numpy as np
 import scipy as sp
+import unittest
 
 
 objective = pypesto.Objective(fun=sp.optimize.rosen,
@@ -21,70 +22,73 @@ for j in range(0, 4):
     result.optimize_result.append(optimizer_result=optimizer_result)
 
 
-def test_waterfall():
-    pypesto.visualize.waterfall(result)
+class TestVisualize(unittest.TestCase):
+
+    def test_waterfall(self):
+        pypesto.visualize.waterfall(result)
+
+    def test_waterfall_lowlevel(self):
+        # test empty input
+        pypesto.visualize.waterfall_lowlevel([])
+
+        # test if it runs at all
+        fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11]
+        pypesto.visualize.waterfall_lowlevel(fvals)
+        fvals = np.array(fvals)
+        pypesto.visualize.waterfall_lowlevel(fvals)
+
+    def test_assign_clusters(self):
+        # test empty input
+        pypesto.visualize.assign_clusters([])
+
+        # test if it runs at all
+        fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11, 10]
+        pypesto.visualize.assign_clusters(fvals)
+        fvals = np.array(fvals)
+        clust, clustsize, ind_clust = pypesto.visualize.assign_clusters(fvals)
+
+        # test if clustering works as intended
+        fvals = [0., 0.00001, 1., 2., 2.001]
+        clust, clustsize, ind_clust = pypesto.visualize.assign_clusters(fvals)
+        self.assertEqual(len(clust), 5)
+        self.assertEqual(len(clustsize), 3)
+        self.assertEqual(len(ind_clust), 5)
+
+    def test_assign_clustered_colors(self):
+        # test empty input
+        pypesto.visualize.assign_clustered_colors([])
+
+        # test if it runs at all
+        fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11]
+        pypesto.visualize.assign_clustered_colors(fvals)
+        fvals = np.array(fvals)
+        pypesto.visualize.assign_clustered_colors(fvals)
+
+    def test_parameters(self):
+        pypesto.visualize.parameters(result)
+
+    def test_parameters_lowlevel(self):
+        # test empty input
+        xs = np.array([])
+        xs.shape = (0, 0)  # we can assume in input that xs.ndim == 2
+        fvals = np.array([])
+        pypesto.visualize.parameters_lowlevel(xs, fvals)
+
+        fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11]
+        xs = [[0.1, 1], [1.2, 3], [2, 4], [1.2, 4.1], [1.1, 3.5],
+              [4.2, 3.5], [1, 4], [6.2, 5], [4.3, 3], [3, 2]]
+
+        # pass lists
+        pypesto.visualize.parameters_lowlevel(xs, fvals, lb=lb, ub=ub)
+
+        # pass numpy arrays
+        fvals = np.array(fvals)
+        xs = np.array(xs)
+        pypesto.visualize.parameters_lowlevel(xs, fvals, lb=lb, ub=ub)
+
+        # test no bounds
+        pypesto.visualize.parameters_lowlevel(xs, fvals)
 
 
-def test_waterfall_lowlevel():
-    # test empty input
-    pypesto.visualize.waterfall_lowlevel([])
-
-    # test if it runs at all
-    fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11]
-    pypesto.visualize.waterfall_lowlevel(fvals)
-    fvals = np.array(fvals)
-    pypesto.visualize.waterfall_lowlevel(fvals)
-
-
-def test_assign_clusters():
-    # test empty input
-    pypesto.visualize.assign_clusters([])
-
-    # test if it runs at all
-    fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11, 10]
-    pypesto.visualize.assign_clusters(fvals)
-    fvals = np.array(fvals)
-    clust, clustsize, ind_clust = pypesto.visualize.assign_clusters(fvals)
-
-    # test if clustering works as intended
-    fvals = [0., 0.00001, 1., 2., 2.001]
-    clust, clustsize, ind_clust = pypesto.visualize.assign_clusters(fvals)
-    assert len(clustsize) == 3
-
-
-def test_assign_clustered_colors():
-    # test empty input
-    pypesto.visualize.assign_clustered_colors([])
-
-    # test if it runs at all
-    fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11]
-    pypesto.visualize.assign_clustered_colors(fvals)
-    fvals = np.array(fvals)
-    pypesto.visualize.assign_clustered_colors(fvals)
-
-
-def test_parameters():
-    pypesto.visualize.parameters(result)
-
-
-def test_parameters_lowlevel():
-    # test empty input
-    xs = np.array([])
-    xs.shape = (0, 0)  # we can assume in input that xs.ndim == 2
-    fvals = np.array([])
-    pypesto.visualize.parameters_lowlevel(xs, fvals)
-
-    fvals = [0.01, 0.02, 1.01, 2.02, 2.03, 2.04, 3, 4, 4.1, 4.11]
-    xs = [[0.1, 1], [1.2, 3], [2, 4], [1.2, 4.1], [1.1, 3.5],
-          [4.2, 3.5], [1, 4], [6.2, 5], [4.3, 3], [3, 2]]
-
-    # pass lists
-    pypesto.visualize.parameters_lowlevel(xs, fvals, lb=lb, ub=ub)
-
-    # pass numpy arrays
-    fvals = np.array(fvals)
-    xs = np.array(xs)
-    pypesto.visualize.parameters_lowlevel(xs, fvals, lb=lb, ub=ub)
-
-    # test no bounds
-    pypesto.visualize.parameters_lowlevel(xs, fvals)
+if __name__ == '__main__':
+    unittest.main()
