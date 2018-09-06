09c6d269c6be2eee58177bf553f3a95c3d9610ee
yannikschaelte <yannik.schaelte@gmail.com>
2018-09-06 15:25:05 +0200

    improve doc

--- pypesto/startpoint/__init__.py
+++ pypesto/startpoint/__init__.py
@@ -18,10 +18,11 @@ lb, ub: ndarray
     values.
 
 x_guesses: ndarray, shape=(g, dim), optional
-    Parameter guesses, where g denotes the number of guesses. Note that
-    these are only possibly taken as reference points to generate new
-    start points, but regardless of g, always n_starts points are
-    generated.
+    Parameter guesses by the user, where g denotes the number of guesses.
+    Note that these are only possibly taken as reference points to generate
+    new start points (e.g. to maximize some distance) depending on the
+    method, but regardless of g, there are always n_starts points generated
+    and returned.
 
 objective: pypesto.Objective, optional
     The objective can be used to evaluate the goodness of start points.
