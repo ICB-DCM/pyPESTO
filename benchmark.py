9852b45da5831bb5280ea88ead13f0355a416d6a
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-09 08:26:34 +0100

    adapt import test scripts

--- benchmark.py
+++ benchmark.py
@@ -12,8 +12,6 @@ import importlib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from pypesto.objective.amici_objective import (import_sbml_model,
-                                               amici_objective_from_measurement_file)
 import petab
 import amici
 
@@ -29,15 +27,16 @@ models = ['Bachmann_MSB2011', 'beer_MolBioSystems2014', 'boehm_JProteomeRes2014'
 
 #model_root = os.path.abspath(os.path.join('Benchmark-Models', 'hackathon_contributions_new_data_format'))
 model_root = '/home/yannik/benchmark-models/hackathon_contributions_new_data_format/'
-
 benchmark_model = 'Zheng_PNAS2012' # 'Zheng_PNAS2012'
+#benchmark_model = "Boehm_JProteomeRes2014"
+#benchmark_model = "Fujita_SciSignal2010"
 condition_filename = os.path.join(model_root, benchmark_model, f'experimentalCondition_{benchmark_model}.tsv')
 measurement_filename = os.path.join(model_root, benchmark_model,f'measurementData_{benchmark_model}.tsv')
 parameter_filename = os.path.join(model_root, benchmark_model, f'parameters_{benchmark_model}.tsv')
 sbml_model_file = os.path.join(model_root, benchmark_model, f'model_{benchmark_model}.xml')
 model_name = f'model_{benchmark_model}'
 model_output_dir = f'deleteme-{model_name}'
-
+ 
 rebuild = False
 #rebuild = True
 if rebuild:
@@ -74,45 +73,19 @@ parameter_df = petab.get_parameter_df(parameter_filename)
 
 # Create objective function instance from model and measurements
 
-petab_problem = petab.OptimizationProblem(sbml_model_file,
-                                    measurement_filename,
-                                    condition_filename,
-                                    parameter_filename)
+manager = petab.Manager.from_folder(model_root + benchmark_model)
+manager.map_par_sim_to_par_opt()
+importer = pypesto.objective.Importer(manager)
+model = importer.model
 
-objective = amici_objective_from_measurement_file(amici_model=model,
-                                                  sbml_model=petab_problem.sbml_model,
-                                                  condition_df=petab_problem.condition_df,
-                                                measurement_df=petab_problem.measurement_df,
-                                                amici_solver=solver)
+print("MODEL PAMS:", list(model.getParameterIds()))
+print("MODEL CONST PAMS:", list(model.getFixedParameterIds()))
+print("MODEL OUTPUTS:", list(model.getObservableIds()))
 
-def get_amici_scaling(scaling_string):
-    if scaling_string == 'lin':
-        return amici.ParameterScaling_none
-    if scaling_string == 'log10':
-        return amici.ParameterScaling_log10
-    if scaling_string == 'log':
-        return amici.ParameterScaling_ln
-    raise ValueError(f'Invalid parameter scaling string {scaling_string}. Must be one of ["lin", "log", "log10"]')
+model.setParameterScale(amici.ParameterScaling_log10)
+obj, edatas = importer.create_objective()
+x_nom = manager.parameter_df['nominalValue'].values
 
-# TODO: need to expose std::vector<ParameterScaling> in AMICI to make this nicer; at the moment we can set only a single one for all parameters?
-pscale = parameter_df.loc[objective.optimization_parameter_ids, 'parameterScale'].apply(get_amici_scaling)
-if np.all(pscale == pscale[0]):
-    if pscale[0] == amici.ParameterScaling_log10:
-        model.setParameterScale(amici.ParameterScaling_log10)
-    elif pscale[0] == amici.ParameterScaling_none:
-        model.setParameterScale(amici.ParameterScaling_none)
-    elif pscale[0] == amici.ParameterScaling_ln:
-        model.setParameterScale(amici.ParameterScaling_ln)
-else:
-    raise ValueError('Currently all parameters must have same parameterScale.')
-#model_parameters = set(model.getParameterIds())
-#parameter_df_parameters = set(parameter_df.index)
-#print("only in model:", model_parameters - parameter_df_parameters)
-#print("only in table", parameter_df_parameters - model_parameters)
-#print("opt param only not in table", set(objective.optimization_parameter_ids) - parameter_df_parameters)
+print(x_nom)
+print("obj: ", obj(x_nom))
 
-# evaluate with nominal parameters
-nominal_x = parameter_df.loc[objective.optimization_parameter_ids, 'nominalValue'].values
-print(nominal_x)
-llh = objective(x=nominal_x)
-print(f'llh: {llh}, lh: {np.exp(llh)}')
