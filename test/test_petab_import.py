b77bb29949d275ebdedf345f19c0d49949ca3355
yannikschaelte <yannik.schaelte@gmail.com>
2019-01-16 16:07:23 +0100

    init test for petab import

---
+++ test/test_petab_import.py
@@ -0,0 +1,31 @@
+
+"""
+This is for testing the petab import.
+"""
+
+import numpy as np
+import scipy as sp
+import numbers
+import pypesto
+import unittest
+
+
+class PetabImportTest(unittest.TestCase):
+
+    def test_compile(self):
+        pass
+
+    def test_simulate(self):
+        pass
+
+    def test_optimize(self):
+        pass
+
+    def test_create_measurement_df_from_rdatas(self):
+        pass
+    
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(ObjectiveTest())
+    unittest.main()
