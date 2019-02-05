b165622de52919e29a53d752ea5d66fb68b81170
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2019-02-05 17:17:22 +0100

    added tests for profile visualization

--- test/test_visualize.py
+++ test/test_visualize.py
@@ -3,14 +3,18 @@ import pypesto.visualize
 import numpy as np
 import scipy as sp
 import unittest
+import copy
 
 
+# define a pypesto problem
 objective = pypesto.Objective(fun=sp.optimize.rosen,
                               grad=sp.optimize.rosen_der,
                               hess=sp.optimize.rosen_hess)
 lb = -7 * np.ones((1, 2))
 ub = 7 * np.ones((1, 2))
 problem = pypesto.Problem(objective=objective, lb=lb, ub=ub)
+
+# write some dummy results for optimization
 result = pypesto.Result(problem=problem)
 for j in range(0, 3):
     optimizer_result = pypesto.OptimizerResult(fval=j * 0.01,
@@ -21,6 +25,22 @@ for j in range(0, 4):
                                                x=[2.5 + j + 0.1, 2 + j + 1])
     result.optimize_result.append(optimizer_result=optimizer_result)
 
+# write some dummy results for profiling
+# initialize profile
+ratio_path_1 = [0.15, 0.25, 0.7, 1., 0.8, 0.35, 0.15]
+ratio_path_2 = [0.1, 0.2, 0.7, 1., 0.8, 0.3, 0.1]
+x_path_1 = np.array([[2., 2.1, 2.3, 2.5, 2.7, 2.9, 3.],
+            [1., 1.2, 1.4, 1.5, 1.6, 1.8, 2.]])
+x_path_2 = np.array([[1., 1.1, 1.3, 1.5, 1.7, 1.9, 2.1],
+            [2.1, 2.2, 2.4, 2.5, 2.8, 2.9, 3.1]])
+fval_path_1 = [4., 3., 1., 0., 1.5, 2.5, 5.]
+fval_path_2 = [4.5, 3.5, 1.5, 0., 1.3, 2.3, 4.3]
+tmp_result_1 = pypesto.ProfilerResult(x_path_1, fval_path_1, ratio_path_1)
+tmp_result_2 = pypesto.ProfilerResult(x_path_2, fval_path_2, ratio_path_2)
+
+result.profile_result.create_new_profile_list()
+result.profile_result.create_new_profile(tmp_result_1)
+result.profile_result.create_new_profile(tmp_result_2)
 
 class TestVisualize(unittest.TestCase):
 
@@ -93,6 +113,33 @@ class TestVisualize(unittest.TestCase):
 
         # test no bounds
         pypesto.visualize.parameters_lowlevel(xs, fvals)
+
+    @staticmethod
+    def test_profiles():
+        pypesto.visualize.profiles(result)
+
+    @staticmethod
+    def test_profiles_lowlevel():
+        # test empty input
+        pypesto.visualize.profiles_lowlevel([])
+
+        # test if it runs at all
+        p1 = np.array([[2., 2.1, 2.3, 2.5, 2.7, 2.9, 3.],
+                       [0.15, 0.25, 0.7, 1., 0.8, 0.35, 0.15]])
+        p2 = np.array([[1., 1.2, 1.4, 1.5, 1.6, 1.8, 2.],
+                       [0.1, 0.2, 0.5, 1., 0.6, 0.4, 0.1]])
+        fvals = [p1, p2]
+        pypesto.visualize.profiles_lowlevel(fvals)
+
+    @staticmethod
+    def test_profile_lowlevel():
+        # test empty input
+        pypesto.visualize.profile_lowlevel([])
+
+        # test if it runs at all
+        fvals = np.array([[2., 2.1, 2.3, 2.5, 2.7, 2.9, 3.],
+                          [0.15, 0.25, 0.7, 1., 0.8, 0.35, 0.15]])
+        pypesto.visualize.profile_lowlevel(fvals)
 
 
 if __name__ == '__main__':
