fa7c12b55a0a11418e3130bf03f9b5e45fb15658
yannikschaelte <yannik.schaelte@gmail.com>
2019-02-08 17:13:35 +0100

    update __init__.py

--- pypesto/__init__.py
+++ pypesto/__init__.py
@@ -5,6 +5,7 @@ pyPESTO
 Parameter Estimation TOolbox for python.
 """
 
+
 from .version import __version__  # noqa: F401
 from .objective import (ObjectiveOptions,
                         Objective,
@@ -24,22 +25,36 @@ from .optimize import (minimize,
 from .profile import (parameter_profile,
                       ProfileOptions,
                       ProfilerResult)
+from .engine import (OptimizerTask,
+                     SingleCoreEngine,
+                     MultiProcessEngine)
 
-__all__ = ["ObjectiveOptions",
-           "Objective",
-           "AmiciObjective",
-           "PetabImporter",
-           "Problem",
-           "Result",
-           "OptimizeResult",
-           "ProfileResult",
-           "SampleResult",
-           "minimize",
-           "OptimizeOptions",
-           "OptimizerResult",
-           "Optimizer",
-           "ScipyOptimizer",
-           "DlibOptimizer",
-           "parameter_profile",
-           "ProfileOptions",
-           "ProfilerResult"]
+
+__all__ = [
+    # objective
+    "ObjectiveOptions",
+    "Objective",
+    "AmiciObjective",
+    "PetabImporter",
+    # problem
+    "Problem",
+    # result
+    "Result",
+    "OptimizeResult",
+    "ProfileResult",
+    "SampleResult",
+    # optimize
+    "minimize",
+    "OptimizeOptions",
+    "OptimizerResult",
+    "Optimizer",
+    "ScipyOptimizer",
+    "DlibOptimizer",
+    # profile
+    "parameter_profile",
+    "ProfileOptions",
+    "ProfilerResult",
+    # engine
+    "SingleCoreEngine",
+    "MultiProcessEngine",
+]
