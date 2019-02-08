878c59d3c2babcc7ee272eb40d63094f19cff6a6
yannikschaelte <yannik.schaelte@gmail.com>
2019-02-08 19:17:52 +0100

    make petab-amici deepcopy and set/getstate work

--- pypesto/objective/petab_import.py
+++ pypesto/objective/petab_import.py
@@ -464,15 +464,45 @@ class PetabAmiciObjective(AmiciObjective):
         self.petab_importer = petab_importer
 
     def __getstate__(self):
-        state = {'petab_importer': self.petab_importer}
+        state = {}
+        for key in set(self.__dict__.keys()) - \
+                set(['amici_model', 'amici_solver', 'edatas',
+                     'preequilibration_edatas']):
+            state[key] = self.__dict__[key]
         return state
 
     def __setstate__(self, state):
+        self.__dict__.update(state)
         petab_importer = state['petab_importer']
-        obj = petab_importer.create_objective()
-        self.__dict__ = obj.__dict__
+        
+        model = petab_importer.create_model()
+        solver = petab_importer.create_solver(model)
+        edatas = petab_importer.create_edatas(model)
+        
+        self.amici_model = model
+        self.amici_solver = solver
+        self.edatas = edatas
+
+        if self.preprocess_edatas:
+            self.init_preequilibration_edatas(edatas)
+        else:
+            self.preequilibration_edatas = None
 
     def __deepcopy__(self, memodict=None):
-        petab_importer = copy.deepcopy(self.petab_importer)
-        other = petab_importer.create_objective()
+        other = self.__class__.__new__(self.__class__)
+
+        for key in set(self.__dict__.keys()) - \
+                set(['amici_model', 'amici_solver', 'edatas',
+                     'preequilibration_edatas']):
+            other.__dict__[key] = copy.deepcopy(self.__dict__[key])
+
+        other.amici_model = amici.ModelPtr(self.amici_model.clone())
+        other.amici_solver = amici.SolverPtr(self.amici_solver.clone())
+        other.edatas = [amici.ExpData(data) for data in self.edatas]
+
+        if self.preprocess_edatas:
+            other.init_preequilibration_edatas(other.edatas)
+        else:
+            other.preequilibration_edatas = None
+
         return other
