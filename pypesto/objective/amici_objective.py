fix sres and cleanup derivative tests

pypesto/objective/amici_objective.py
@@ -371,7 +371,7 @@ class AmiciObjective(Objective):
                         self.x_ids,
                         self.mapping_par_opt_to_par_sim[data_ix],
                         rdata['sres'],
-                        coefficient=-1.0
+                        coefficient=1.0
                     )
                     sres = np.vstack([sres, opt_sres]) \
                         if sres.size else opt_sres
@@ -404,7 +404,7 @@ class AmiciObjective(Objective):
             FVAL: np.inf,
             GRAD: np.nan * np.ones(self.dim),
             HESS: np.nan * np.ones([self.dim, self.dim]),
-            RES: np.nan * np.ones(n_res),
+            RES:  np.nan * np.ones(n_res),
             SRES: np.nan * np.ones([n_res, self.dim]),
             RDATAS: rdatas
         }

test/test_sbml_conversion.py
@@ -18,6 +18,8 @@ optimizers = {
     'dlib': ['default']
 }
 
+ATOL = 1e-2
+RTOL = 1e-3
 
 class AmiciObjectiveTest(unittest.TestCase):
     def runTest(self):
@@ -26,24 +28,24 @@ class AmiciObjectiveTest(unittest.TestCase):
             x0 = list(model.getParameters())
             df = objective.check_grad(
                 x0,
-                eps=1e-5,
+                eps=1e-3,
                 verbosity=0,
                 mode=pypesto.objective.constants.MODE_FUN
             )
             print("relative errors MODE_FUN: ", df.rel_err.values)
             print("absolute errors MODE_FUN: ", df.abs_err.values)
-            self.assertTrue(np.all(df.rel_err.values < 1e-2))
-            self.assertTrue(np.all(df.abs_err.values < 1e-1))
+            self.assertTrue(np.all((df.rel_err.values < RTOL) |
+                                   (df.abs_err.values < ATOL)))
             df = objective.check_grad(
                 x0,
-                eps=1e-5,
+                eps=1e-3,
                 verbosity=0,
                 mode=pypesto.objective.constants.MODE_RES
             )
             print("relative errors MODE_RES: ", df.rel_err.values)
             print("absolute errors MODE_RES: ", df.rel_err.values)
-            self.assertTrue(np.all(df.rel_err.values < 1e-2))
-            self.assertTrue(np.all(df.abs_err.values < 1e-2))
+            self.assertTrue(np.all((df.rel_err.values < RTOL) |
+                                   (df.abs_err.values < ATOL)))
 
             for library in optimizers.keys():
                 for method in optimizers[library]: