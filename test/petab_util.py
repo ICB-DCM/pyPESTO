ef635514b5c6de308b3e99f3f3a87d0fb964e480
yannikschaelte <yannik.schaelte@gmail.com>
2020-02-20 22:41:23 +0100

    test

--- test/petab_util.py
+++ test/petab_util.py
@@ -1,12 +1,20 @@
 import git
+import os
 
 
 repo_base = "doc/example/tmp/benchmark-models/"
+
+if not os.path.exists(repo_base):
+    git.Repo.clone_from(
+        "git://github.com/LeonardSchmiester/Benchmark-Models.git",
+        repo_base, depth=1)
+g = git.Git(repo_base)
+
+# update repo if online
 try:
-    git.Git().clone("git://github.com/LeonardSchmiester/Benchmark-Models.git",
-                    repo_base, depth=10)
-except Exception:
-    git.Git(repo_base).pull()
+    g.pull()
+except git.exc.GitCommandError:
+    pass
 
 # model folder base
 folder_base = repo_base + "hackathon_contributions_new_data_format/"
