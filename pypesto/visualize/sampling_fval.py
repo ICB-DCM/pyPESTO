119fc23d5d0a228663a41172d47fa6a75c60f5ac
erikadudki <edudkin@uni-bonn.de>
2020-03-24 11:40:09 +0100

    add simple plotting function for fval and parameters

--- pypesto/visualize/sampling_fval.py
+++ pypesto/visualize/sampling_fval.py
@@ -1,8 +1,33 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-def sampling_fval(results, ax=None,
-               size=None, reference=None, colors=None, legends=None,
-               balance_alpha=True, start_indices=None):
+
+def x_as_ndarray(result):
+    """Creates numpy array of size (n_chain, n_sample, n_par).
+    Returns ndarray of parameters and fval
+    """
+    n_chain = len(result['chains'])
+    n_sample = len(result['chains'][0])
+    n_par = len(result['chains'][0][0]['x'])
+
+    # reserve space
+    arr = np.zeros((n_chain, n_sample, n_par))
+    arr_fval = np.zeros((n_sample, 1))
+
+    # fill
+    for i_chain, chain in enumerate(result['chains']):
+        for i_sample, sample in enumerate(chain):
+            arr[i_chain, i_sample, :] = sample['x']
+
+    for i_sample in range(0, n_sample):
+        arr_fval[i_sample] = result['chains'][0][i_sample]['fval']
+
+    return arr, arr_fval
+
+
+def sampling_fval(result, size=None, fs = 12, noex_param_name = True):
     """
     Plot parameter values.
 
@@ -12,32 +37,15 @@ def sampling_fval(results, ax=None,
     results: pypesto.Result or list
         Optimization result obtained by 'optimize.py' or list of those
 
-    ax: matplotlib.Axes, optional
-        Axes object to use.
-
     size: tuple, optional
         Figure size (width, height) in inches. Is only applied when no ax
         object is specified
 
-    reference: list, optional
-        List of reference points for optimization results, containing et
-        least a function value fval
+    fs: int
+        fontsize for the plots
 
-    colors: list, or RGBA, optional
-        list of colors, or single color
-        color or list of colors for plotting. If not set, clustering is done
-        and colors are assigned automatically
-
-    legends: list or str
-        Labels for line plots, one label per result object
-
-    balance_alpha: bool (optional)
-        Flag indicating whether alpha for large clusters should be reduced to
-        avoid overplotting (default: True)
-
-    start_indices: list or int
-        list of integers specifying the multistarts to be plotted or
-        int specifying up to which start index should be plotted
+    noex_param_name: bool
+        if parameter names exist set: noex_param_name = False
 
     Returns
     -------
@@ -45,25 +53,37 @@ def sampling_fval(results, ax=None,
     ax: matplotlib.Axes
         The plot axes.
     """
+#
+    Version2 = True     # results format from Dilan
 
-    def x_as_ndarray(result):
-        """Creates numpy array of size (n_chain, n_sample, n_par).
-        Returns ndarray of parameters and fval
-        """
-        n_chain = len(result['chains'])
-        n_sample = len(result['chains'][0])
-        n_par = len(result['chains'][0][0]['x'])
+    # get parameters and fval results as numpy-arrays
+    if Version2:
+        arr = np.transpose(result['theta'])
+        arr_fval = result['log_posterior']
+    else:
+        arr, arr_fval = x_as_ndarray(result)
+        arr = arr[0]
 
-        # reserve space
-        arr = np.zeros((n_chain, n_sample, n_par))
-        arr_fval = np.zeros((n_sample, 1))
+    # plot fval
+    ax = plt.subplots(figsize =size)[1]
+    x_fval = range(1,len(arr_fval)+1)
+    plt.scatter(x_fval, arr_fval, alpha=0.5, s=10)
+    ax.set_xlabel('number of interations', fontsize = fs)
+    ax.set_ylabel('log Posterior', fontsize = fs)
+    ax.tick_params(axis='both', which='major', labelsize=fs)
+    plt.show()
 
-        # fill
-        for i_chain, chain in enumerate(result['chains']):
-            for i_sample, sample in enumerate(chain):
-                arr[i_chain, i_sample, :] = sample['x']
+    # plot parameters
+    sns.set(style="ticks")
+    # transform nparray to pandas for the use of seaborn
+    if noex_param_name:
+        param_names = []
+        for i_param in range(0,np.shape(arr)[1]):
+            param_names = np.append(param_names,'par' + str(i_param))
 
-        for i_sample in range(0, n_sample):
-            arr_fval[i_sample] = result['chains'][0][i_sample]['fval']
+    pd_array = pd.DataFrame(arr, columns = param_names)
+    sns.pairplot(pd_array)
 
-        return arr, arr_fval
+    return ax
+
+# sampling_fval(result, size=None, fs = 12, noex_param_name = True)
