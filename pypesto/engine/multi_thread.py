22f73fba3908f87f6d4f28cfa76f483a020441b3
yannikschaelte <yannik.schaelte@gmail.com>
2020-03-27 21:30:27 +0100

    fix variable name

--- pypesto/engine/multi_thread.py
+++ pypesto/engine/multi_thread.py
@@ -39,7 +39,7 @@ class MultiThreadEngine(Engine):
                 f"Engine set up to use up to {n_threads} processes in total. "
                 f"The number was automatically determined and might not be "
                 f"appropriate on some systems.")
-        self.n_procs: int = n_threads
+        self.n_threads: int = n_threads
 
     def execute(self, tasks: List[Task]):
         """Deepcopy tasks and distribute work over parallel threads."""
@@ -47,10 +47,10 @@ class MultiThreadEngine(Engine):
 
         copied_tasks = [copy.deepcopy(task) for task in tasks]
 
-        n_procs = min(self.n_procs, n_tasks)
-        logger.info(f"Performing parallel task execution on {n_procs} "
+        n_threads = min(self.n_threads, n_tasks)
+        logger.info(f"Performing parallel task execution on {n_threads} "
                     f"threads.")
-        with ThreadPoolExecutor(max_workers=n_procs) as pool:
+        with ThreadPoolExecutor(max_workers=n_threads) as pool:
             results = pool.map(work, copied_tasks)
 
         return results
