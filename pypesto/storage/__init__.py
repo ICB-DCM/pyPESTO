273a2b8e1c6cf3d3d305136b8a37b3c8b387139b
Polina Lakrisenko <p.lakrisenko@gmail.com>
2020-03-30 20:58:49 +0200

    writing problem and result to hdf5

--- pypesto/storage/__init__.py
+++ pypesto/storage/__init__.py
@@ -4,4 +4,4 @@ Storage
 
 """
 
-from .save_to_hdf5 import save_to_hdf5
+from .save_to_hdf5 import ProblemHDF5Writer, OptimizationResultHDF5Writer
--- pypesto/storage/save_to_hdf5.py
+++ pypesto/storage/save_to_hdf5.py
@@ -1,52 +1,57 @@
 import h5py
+import numpy as np
 from .hdf5 import write_string_array, write_int_array, write_float_array
 from ..result import Result
-from ..objective.constants import *
 
 
-def save_problem(f, problem):
-    problem_grp = f.create_group("problem")
-    # problem_grp.attrs['config'] = objective.get_config()
-    problem_grp.attrs['dim'] = problem.dim
-    problem_grp.attrs['dim_full'] = problem.dim_full
+class ProblemHDF5Writer:
+    LB = 'lb'
+    UB = 'ub'
+    LB_FULL = 'lb_full'
+    UB_FULL = 'ub_full'
+    X_FIXED_VALUES = 'x_fixed_values'
+    X_FIXED_INDICES = 'x_fixed_indices'
+    X_NAMES = 'x_names'
+    DIM = 'dim'
+    DIM_FULL = 'dim_full'
 
-    write_float_array(problem_grp, "lb", problem.lb)
-    write_float_array(problem_grp, "ub", problem.ub)
-    write_float_array(problem_grp, "lb_full", problem.lb_full)
-    write_float_array(problem_grp, "ub_full", problem.ub_full)
-    write_float_array(problem_grp, "x_fixed_values", problem.x_fixed_vals)
-    write_int_array(problem_grp, "x_fixed_indices", problem.x_fixed_indices)
-    write_string_array(problem_grp, "x_names", problem.x_names)
+    def __init__(self, storage_filename):
+        self.storage_filename = storage_filename
 
+    def write(self, problem):
+        with h5py.File(self.storage_filename, "a") as f:
+            problem_grp = f.create_group("problem")
+            # problem_grp.attrs['config'] = objective.get_config()
+            problem_grp.attrs[self.DIM] = problem.dim
+            problem_grp.attrs[self.DIM_FULL] = problem.dim_full
 
-def save_optimization(f, result):
-    optimization_grp = f.create_group("optimization")
-    # settings = optimization_grp.create_dataset("settings", settings, dtype=)
-    results_grp = optimization_grp.create_group("results")
-    for i, start in enumerate(result.list):
-        start_grp = results_grp.create_group(str(i))
-
-        start_grp.attrs[FVAL] = start.fval
-        start_grp.attrs[N_FVAL] = start.n_fval
-        start_grp.attrs[N_GRAD] = start.n_grad
-        start_grp.attrs['fval0'] = start.fval0
-        start_grp.attrs['exitflag'] = start.exitflag
-        start_grp.attrs['time'] = start.time
-        start_grp.attrs['message'] = start.message
-
-        write_float_array(start_grp, X, start.x)
-        write_float_array(start_grp, GRAD, start.grad)
-        if start.hess:
-            write_float_array(start_grp, HESS, start.hess)
-        write_float_array(start_grp, X0, start.x0)
+            write_float_array(problem_grp, self.LB, problem.lb)
+            write_float_array(problem_grp, self.UB, problem.ub)
+            write_float_array(problem_grp, self.LB_FULL, problem.lb_full)
+            write_float_array(problem_grp, self.UB_FULL, problem.ub_full)
+            write_float_array(problem_grp, self.X_FIXED_VALUES,
+                              problem.x_fixed_vals)
+            write_int_array(problem_grp, self.X_FIXED_INDICES,
+                            problem.x_fixed_indices)
+            write_string_array(problem_grp, self.X_NAMES, problem.x_names)
 
 
-def save_to_hdf5(result: Result,
-                 output_file: str):
-    with h5py.File(output_file, "w") as f:
-        save_problem(f, result.problem)
-        save_optimization(f, result.optimize_result)
+class OptimizationResultHDF5Writer:
 
+    def __init__(self, storage_filename):
+        self.storage_filename = storage_filename
 
-def save_to_csv():
-    pass
+    def write(self, result: Result):
+        with h5py.File(self.storage_filename, "a") as f:
+            optimization_grp = f.create_group("optimization")
+            # settings =
+            # optimization_grp.create_dataset("settings", settings, dtype=)
+            results_grp = optimization_grp.create_group("results")
+            for i, start in enumerate(result.optimize_result.list):
+                start_grp = results_grp.create_group(str(i))
+
+                for key in start.keys():
+                    if isinstance(start[key], np.ndarray):
+                        write_float_array(start_grp, key, start[key])
+                    elif start[key] is not None:
+                        start_grp.attrs[key] = start[key]
