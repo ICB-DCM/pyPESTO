4895c1c0bb811bced7bab182d33cc3a4f62ba71d
LeonardSchmiester <leonard.schmiester@helmholtz-muenchen.de>
2020-03-31 10:30:00 +0200

    added docstring

--- pypesto/storage/read_from_hdf5.py
+++ pypesto/storage/read_from_hdf5.py
@@ -5,7 +5,19 @@ from ..optimize.result import OptimizerResult
 
 def read_hdf5_optimization(f: h5py.File,
                            start: h5py.Group) -> 'OptimizerResult':
+    """
+    Read HDF5 results per start.
+
+    Parameters
+    ----------
+    f:
+        The HDF5 result file
+    start:
+        Specifies the start that is read from the HDF5 file
+    """
+
     result = OptimizerResult()
+
     for optimization_key in f[f'/optimization/results/{start}']:
         if optimization_key in result.keys():
             result[optimization_key] = \
@@ -14,12 +26,22 @@ def read_hdf5_optimization(f: h5py.File,
 
 
 class OptimizationResultHDF5Reader:
+    """
+    Reader of the HDF5 result files written by class OptimizationResultHDF5Writer.
 
-    def __init__(self, storage_filename):
+    Attributes
+    ---------
+    storage_filename:
+        HDF5 result file name
+    """
+    def __init__(self, storage_filename: str):
         self.storage_filename = storage_filename
         self.results = OptimizeResult()
 
     def read(self) -> 'OptimizeResult':
+        """
+        Read HDF5 result file and return pyPESTO result object.
+        """
         with h5py.File(self.storage_filename, "r") as f:
             for start in f['/optimization/results']:
                 result = read_hdf5_optimization(f, start)
