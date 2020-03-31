9f664ffc78123114a7515086e12d4f58c384a3d0
LeonardSchmiester <leonard.schmiester@helmholtz-muenchen.de>
2020-03-31 09:31:24 +0200

    Added basic hdf5 reader

---
+++ pypesto/storage/read_from_hdf5.py
@@ -0,0 +1,26 @@
+import h5py
+from ..result import OptimizeResult
+from ..optimize.result import OptimizerResult
+
+
+def read_hdf5_optimization(f: h5py.File,
+                           start: h5py.Group) -> 'OptimizerResult':
+    result = OptimizerResult()
+    for optimization_key in f[f'/optimization/results/{start}']:
+        if optimization_key in result.keys():
+            result[optimization_key] = f[f'/optimization/results/{start}/{optimization_key}']
+    return result
+
+
+class OptimizationResultHDF5Reader:
+
+    def __init__(self, storage_filename):
+        self.storage_filename = storage_filename
+        self.results = OptimizeResult()
+
+    def read(self) -> 'OptimizeResult':
+        with h5py.File(self.storage_filename, "r") as f:
+            for start in f['/optimization/results']:
+                result = read_hdf5_optimization(f, start)
+                self.results.append(result)
+        return self.results
