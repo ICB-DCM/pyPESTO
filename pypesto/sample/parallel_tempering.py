6d8224b2494932b0972699c4456953efa4b7392d
yannikschaelte <yannik.schaelte@gmail.com>
2020-04-08 15:15:59 +0200

    implement adaptive PT

--- pypesto/sample/parallel_tempering.py
+++ pypesto/sample/parallel_tempering.py
@@ -1,58 +1,68 @@
-from typing import Any, Dict, Sequence, Union
+from typing import Dict, Sequence
 import numpy as np
 import copy
 
 from ..problem import Problem
-from .sampler import Sampler, TemperableSampler
+from .sampler import Sampler, InternalSampler
 from .result import McmcPtResult
 
 
 class ParallelTemperingSampler(Sampler):
+    """Simple parallel tempering sampler."""
 
     def __init__(
             self,
-            internal_sampler: TemperableSampler,
+            internal_sampler: InternalSampler,
+            betas: Sequence[float] = None,
+            n_chains: int = None,
             options: Dict = None):
-        self.internal_sampler = internal_sampler
-        self.options = ParallelTemperingSampler.translate_options(options)
-        self.samplers = [copy.deepcopy(self.internal_sampler)
-                         for _ in range(self.options['n_chains'])]
-        self.betas = np.array(self.options['betas'])
+        super().__init__(options)
+
+        # set betas
+        if (betas is None) + (n_chains is None) != 1:
+            raise ValueError("Set either betas or n_chains.")
+        if betas is None:
+            betas = near_exponential_decay_betas(
+                n_chains=n_chains, exponent=self.options['exponent'],
+                max_temp=self.options['max_temp'])
+        if betas[0] != 1.:
+            raise ValueError("The first chain must have beta=1.0")
+        self.betas0 = np.array(betas)
+        self.betas = None
+
+        self.samplers = [copy.deepcopy(internal_sampler)
+                         for _ in range(len(self.betas0))]
 
     @classmethod
-    def translate_options(cls, options):
-        used_options = {
-            'n_chains': 3,
-            'betas': [1/1e0, 1/1e1, 1/1e3],
+    def default_options(cls) -> Dict:
+        return {
+            'max_temp': 5e4,
+            'exponent': 4,
         }
-        if options is None:
-            options = {}
-        for key, val in options:
-            if key not in used_options:
-                raise KeyError(f"Cannot handle key {key}.")
-            used_options[key] = val
-        if used_options['n_chains'] != len(used_options['betas']):
-            raise AssertionError(
-                "Numbers of chains and temperatures do not match.")
-        return used_options
 
     def initialize(self, problem: Problem, x0: np.ndarray):
+        # initialize all samplers
         for sampler in self.samplers:
             _problem = copy.deepcopy(problem)
             sampler.initialize(_problem, x0)
+        self.betas = self.betas0
 
     def sample(
             self, n_samples: int, beta: float = 1.):
         # loop over iterations
-        for _ in range(int(n_samples)):
+        for i_sample in range(int(n_samples)):
             # sample
             for sampler, beta in zip(self.samplers, self.betas):
                 sampler.sample(n_samples=1, beta=beta)
 
             # swap samples
-            ParallelTemperingSampler.swap_samples(self.samplers, self.betas)
+            swapped = self.swap_samples()
+
+            # adjust temperatures
+            self.adjust_betas(i_sample, swapped)
 
     def get_samples(self) -> McmcPtResult:
+        """Concatenate all chains."""
         results = [sampler.get_samples() for sampler in self.samplers]
         trace_x = np.array([result.trace_x[0] for result in results])
         trace_fval = np.array([result.trace_fval[0] for result in results])
@@ -62,23 +72,65 @@ class ParallelTemperingSampler(Sampler):
             betas=self.betas
         )
 
-    @staticmethod
-    def swap_samples(
-            samplers: Sequence[TemperableSampler],
-            betas: np.ndarray):
-        """Swap samples."""
-        n_chains = len(betas)
-        if n_chains == 1:
-            return
-        dbetas = betas[:-1] - betas[1:]
+    def swap_samples(self) -> Sequence[bool]:
+        """Swap samples as in Vousden2016."""
+        # for recording swaps
+        swapped = []
+
+        if len(self.betas) == 1:
+            # nothing to be done
+            return swapped
+
+        # beta differences
+        dbetas = self.betas[:-1] - self.betas[1:]
+
+        # loop over chains from highest temperature down
         for dbeta, sampler1, sampler2 in reversed(
-                list(zip(dbetas, samplers[:-1], samplers[1:]))):
+                list(zip(dbetas, self.samplers[:-1], self.samplers[1:]))):
+            # extract samples
             sample1 = sampler1.get_last_sample()
             sample2 = sampler2.get_last_sample()
-            p_acc_swap = dbeta * ( sample2.llh - sample1.llh)
 
+            # swapping probability
+            p_acc_swap = dbeta * (sample2.llh - sample1.llh)
+
+            # flip a coin
             u = np.random.uniform(0, 1)
-            if np.log(u) < p_acc_swap:
+
+            # check acceptance
+            swap = np.log(u) < p_acc_swap
+            if swap:
+                # swap
                 sampler2.set_last_sample(sample1)
                 sampler1.set_last_sample(sample2)
-                # TODO implement loggers
+
+            # record
+            swapped.insert(0, swap)
+        return swapped
+
+    def adjust_betas(self, i_sample: int, swapped: Sequence[bool]):
+        """Adjust temperature values. Default: Do nothing."""
+
+
+def near_exponential_decay_betas(
+        n_chains: int, exponent: float, max_temp: float) -> np.ndarray:
+    """Initialize betas in a near-exponential decay scheme.
+
+    Parameters
+    ----------
+    n_chains:
+        Number of chains to use.
+    exponent:
+        Decay exponent. The higher, the more small temperatures are used.
+    max_temp:
+        Maximum chain temperature.
+    """
+    # special case of one chain
+    if n_chains == 1:
+        return np.array([1.])
+
+    temperatures = np.linspace(1, max_temp ** (1 / exponent), n_chains) \
+                   ** exponent
+    betas = 1 / temperatures
+
+    return betas
