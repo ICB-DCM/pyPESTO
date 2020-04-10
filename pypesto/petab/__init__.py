2aa37d703f09120e8ba9eb1ae13694e408adb3c8
yannikschaelte <yannik.schaelte@gmail.com>
2020-04-10 21:26:23 +0200

    tidy up

--- pypesto/petab/__init__.py
+++ pypesto/petab/__init__.py
@@ -7,6 +7,6 @@ Code related to giving pyPESTO support for the PEtab data format.
 
 # PEtab is an optional dependency
 try:
-    from .petab_importer import PetabImporter
+    from .importer import PetabImporter
 except ModuleNotFoundError:
     PetabImporter = None
