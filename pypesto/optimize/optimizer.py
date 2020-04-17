7688a3a03f77d653d14133a3cf96e814e9740afb
LeonardSchmiester <leonard.schmiester@helmholtz-muenchen.de>
2020-04-17 10:32:31 +0200

    fix flake8

--- pypesto/optimize/optimizer.py
+++ pypesto/optimize/optimizer.py
@@ -234,7 +234,7 @@ class ScipyOptimizer(Optimizer):
         if self.options is None:
             self.options = ScipyOptimizer.get_default_options()
             self.options['ftol'] = tol
-        elif not self.options is None and self.options['ftol'] is None:
+        elif self.options is not None and self.options['ftol'] is None:
             self.options['ftol'] = tol
 
     @fix_decorator
