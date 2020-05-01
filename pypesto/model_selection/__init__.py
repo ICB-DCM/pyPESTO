ded3c16282ca2f13f20b331dde5140b94377249b
dilpath <dilan.private+github@outlook.com>
2020-05-01 23:32:37 +0200

    intermediate changes -- revert later

--- pypesto/model_selection/__init__.py
+++ pypesto/model_selection/__init__.py
@@ -6,3 +6,9 @@ Model Selection
 """
 
 from .model_selection import ModelSelectionProblem, ModelSelection
+#from .model_selection import (
+#    ModelCandidate,
+#    ModelSelector,
+#    ForwardSelector,
+#    ModelSelectionProblem
+#)
--- test/test_model_selection.py
+++ test/test_model_selection.py
@@ -5,6 +5,12 @@ import tempfile
 import numpy as np
 from pypesto.model_selection import (ModelSelectionProblem, ModelSelection)
 
+from pypesto.model_selection import (
+    ModelSelectionHelper,
+    ModelSelector,
+    ForwardSelector,
+    ModelSelectionProblem
+)
 
 def test_model_selection():
     # model_selection.tsv
@@ -12,3 +18,9 @@ def test_model_selection():
     model_selection = ModelSelection(petab_problem, 'model_selection.tsv')
     # sm = model_selection.get_smallest_order_problem()
     res = model_selection.forward_selection()
+
+def test_select_model()
+    petab_problem = None
+    model_specification_file = 'model_selection.tsv'
+    selector = ModelSelector(petab_problem, model_specification_file)
+    result = selector.select('forward', 'AIC')
