5b918cb289a007bc3fc992462e7374329c6cf960
Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>
2020-05-03 21:21:37 +0200

    write amici solver settings to stream (#308)
    * write amici solver settings to stream
    
    * fix sampling test failure

--- pypesto/objective/amici_objective.py
+++ pypesto/objective/amici_objective.py
@@ -271,9 +271,14 @@ class AmiciObjective(Objective):
                 {'amici_model', 'amici_solver', 'edatas'}:
             state[key] = self.__dict__[key]
 
+        # write amici solver settings to file
         amici_solver_file = tempfile.mkstemp()[1]
         amici.writeSolverSettingsToHDF5(self.amici_solver, amici_solver_file)
-        state['amici_solver_settings'] = amici_solver_file
+        # read in byte stream
+        amici_solver_settings = open(amici_solver_file, 'rb').read()
+        state['amici_solver_settings'] = amici_solver_settings
+        # remove temporary file
+        os.remove(amici_solver_file)
 
         return state
 
@@ -291,15 +296,20 @@ class AmiciObjective(Objective):
         edatas = self.amici_object_builder.create_edatas(model)
 
         try:
-            amici.readSolverSettingsFromHDF5(
-                state['amici_solver_settings'], solver)
+            # write solver settings to temporary file
+            _file = tempfile.mkstemp()[1]
+            with open(_file, 'wb') as f:
+                f.write(state['amici_solver_settings'])
+            # read in solver settings
+            amici.readSolverSettingsFromHDF5(_file, solver)
+            # remove temporary file
+            os.remove(_file)
         except AttributeError as err:
             if not err.args:
                 err.args = ('',)
             err.args = err.args + ("Amici must have been compiled with hdf5 "
                                    "support",)
             raise
-        os.remove(state['amici_solver_settings'])
 
         self.amici_model = model
         self.amici_solver = solver
