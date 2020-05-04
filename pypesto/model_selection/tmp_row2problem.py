0d32fbbe80cba60b4b845776d41e5087f159406a
dilpath <dilan.private+github@outlook.com>
2020-05-04 16:48:55 +0200

    Some additional functionality -- untested

---
+++ pypesto/model_selection/tmp_row2problem.py
@@ -0,0 +1,71 @@
+import petab
+from colorama import Fore
+from typing import Union
+import numpy as np
+from ..petab import PetabImporter
+from ..objective import Objective
+from ..problem import Problem
+import pandas as pd
+
+from petab.C import NOMINAL_VALUE, ESTIMATE
+
+YAML_FILENAME_COLUMN = "SBML"
+MODEL_NAME_COLUMN = "ModelId"
+
+
+def row2problem(row: dict,
+                petab_problem: Union[petab.Problem, str] = None,
+                obj: Objective = None) -> Problem:
+    """
+    Create a pypesto.Problem from a single, unambiguous model selection row.
+    Optional petab.Problem and objective function can be provided to overwrite
+    model selection yaml entry and default PetabImporter objective
+    respectively.
+    
+    Parameters
+    ----------
+    row:
+        A single, unambiguous model selection row.
+    petab_problem:
+        The petab problem for which to perform model selection.
+    obj:
+        The objective to modify for model selection.
+    
+    Returns
+    -------
+    problem:
+        The problem containing correctly fixed parameter values.
+    """
+    # overwrite petab_problem by problem in case it refers to yaml
+    if petab_problem is None:
+        petab_problem = row[YAML_FILENAME_COLUMN].str
+    if isinstance(petab_problem, str):
+        petab_problem = petab.Problem.from_yaml(petab_problem)
+    importer = PetabImporter(petab_problem)
+
+    # drop row entries not referring to parameters
+    for key in [YAML_FILENAME_COLUMN, MODEL_NAME_COLUMN]:
+        row.pop(key)
+
+    for par_id, par_val in row.items():
+        if par_id not in petab_problem.x_ids:
+            print(Fore.YELLOW + f'Warning: parameter {par_id} is not defined '
+                                f'in PETab model. It will be ignored.')
+            continue
+        if not np.isnan(par_val):
+            petab_problem.parameter_df[ESTIMATE].loc[par_id] = 0
+            petab_problem.parameter_df[NOMINAL_VALUE].loc[par_id] = par_val
+            # petab_problem.parameter_df.lowerBound.loc[par_id] = float("NaN")
+            # petab_problem.parameter_df.upperBound.loc[par_id] = float("NaN")
+        else:
+            petab_problem.parameter_df[ESTIMATE].loc[par_id] = 1
+            # petab_problem.parameter_df.nominalValue.loc[par_id] = float(
+            # "NaN")
+
+    # chose standard objective in case none is provided
+    importer = PetabImporter(petab_problem)
+    if obj is None:
+        obj = importer.create_objective()
+    pypesto_problem = importer.create_problem(obj)
+
+    return pypesto_problem
--- test/test_model_selection.py
+++ test/test_model_selection.py
@@ -3,24 +3,57 @@ This is for testing the pypesto.model_selection.
 """
 import tempfile
 import numpy as np
-from pypesto.model_selection import (ModelSelectionProblem, ModelSelection)
+#from pypesto.model_selection import (ModelSelectionProblem, ModelSelection)
 
 from pypesto.model_selection import (
-    ModelSelectionHelper,
     ModelSelector,
-    ForwardSelector,
-    ModelSelectionProblem
+    ModelSelectionProblem,
+    unpack_file
 )
 
-def test_model_selection():
-    # model_selection.tsv
-    petab_problem = None
-    model_selection = ModelSelection(petab_problem, 'model_selection.tsv')
-    # sm = model_selection.get_smallest_order_problem()
-    res = model_selection.forward_selection()
+#def test_model_selection():
+#    # model_selection.tsv
+#    petab_problem = None
+#    model_selection = ModelSelection(petab_problem, 'model_selection.tsv')
+#    # sm = model_selection.get_smallest_order_problem()
+#    res = model_selection.forward_selection()
+#
+#def test_select_model():
+#    petab_problem = None
+#    model_specification_file = 'model_selection.tsv'
+#    selector = ModelSelector(petab_problem, model_specification_file)
+#    result = selector.select('forward', 'AIC')
 
-def test_select_model()
-    petab_problem = None
-    model_specification_file = 'model_selection.tsv'
-    selector = ModelSelector(petab_problem, model_specification_file)
-    result = selector.select('forward', 'AIC')
+
+ms_file_text = '''ModelId\tSBML\tp1\tp2\tp3
+m0\tsbml1.xml\t0;5;-\t0;-\t-
+m1\tsbml1.xml\t0\t-\t-
+m2\tsbml2.xml\t3;-\t-\t-
+m3\tsbml2.xml\t-\t0;2\t-
+'''
+
+ms_file_text_unpacked = '''ModelId\tSBML\tp1\tp2\tp3
+m0_0\tsbml1.xml\t0\t0\tnan
+m0_1\tsbml1.xml\t0\tnan\tnan
+m0_2\tsbml1.xml\t5\t0\tnan
+m0_3\tsbml1.xml\t5\tnan\tnan
+m0_4\tsbml1.xml\tnan\t0\tnan
+m0_5\tsbml1.xml\tnan\tnan\tnan
+m1_0\tsbml1.xml\t0\tnan\tnan
+m2_0\tsbml2.xml\t3\tnan\tnan
+m2_1\tsbml2.xml\tnan\tnan\tnan
+m3_0\tsbml2.xml\tnan\t0\tnan
+m3_1\tsbml2.xml\tnan\t2\tnan
+'''
+
+def test_get_next_step_candidates():
+    ms_file = tempfile.NamedTemporaryFile(mode='r+', delete=False)
+    with open(ms_file.name, 'w') as f:
+        f.write(ms_file_text)
+    ms_file_unpacked = unpack_file(ms_file.name)
+    print(ms_file_unpacked.read())
+    print(ms_file_text_unpacked)
+    #assert str(ms_file_unpacked.read()) == ms_file_text_unpacked
+    selector = ModelSelector(None, ms_file_unpacked.name)
+    #assert False, selector.header
+
