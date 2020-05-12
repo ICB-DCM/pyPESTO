86c6fef99d9c1a1f675a3db782cab2f0df5f27c6
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2020-05-12 09:57:16 +0200

    log acceptance tempering the likelihood

--- pypesto/sampling/metropolis.py
+++ pypesto/sampling/metropolis.py
@@ -36,18 +36,18 @@ class MetropolisSampler(InternalSampler):
     def sample(self, n_samples: int, beta: float = 1.):
         # load last recorded particle
         x = self.trace_x[-1]
-        llh = - self.trace_fval[-1]
+        lpost = - self.trace_fval[-1]
 
         # loop over iterations
         for _ in range(int(n_samples)):
             # perform step
-            x, llh = self._perform_step(x, llh, beta)
+            x, lpost = self._perform_step(x, lpost, beta)
 
             # record step
             self.trace_x.append(x)
-            self.trace_fval.append(-llh)
+            self.trace_fval.append(-lpost)
 
-    def _perform_step(self, x: np.ndarray, llh: float, beta: float):
+    def _perform_step(self, x: np.ndarray, lpost: float, beta: float):
         """
         Perform a step: Propose new parameter, evaluate and check whether to
         accept.
@@ -59,44 +59,48 @@ class MetropolisSampler(InternalSampler):
         if any(x_new < self.problem.lb) or any(x_new > self.problem.ub):
             # will not be accepted
             llh_new = - np.inf
+            prior_new = - np.inf
         else:
-            # compute function value
+            # compute likelihood value
             llh_new = - self.objective(x_new)
+            prior_new = - self.objective(x_new) # TODO
 
-        # log acceptance probability
-        log_p_acc = min(beta * (llh_new - llh), 0)
-
+        # extract current likelihood value
+        llh = lpost - prior # TODO
+        # log acceptance probability (temper only likelihood)
+        log_p_acc = min(beta * (llh_new - llh) + (prior_new - prior), 0)
         # flip a coin
         u = np.random.uniform(0, 1)
         # check acceptance
         if np.log(u) < log_p_acc:
             # update particle
             x = x_new
-            llh = llh_new
+            # llh = llh_new
+            lpost = llh_new + prior # TODO
 
         # update proposal
-        self._update_proposal(x, llh, log_p_acc, len(self.trace_fval)+1)
+        self._update_proposal(x, log_p_acc, len(self.trace_fval)+1)
 
-        return x, llh
+        return x, lpost
 
     def _propose_parameter(self, x: np.ndarray):
         """Propose a step."""
         x_new: np.ndarray = x + self.options['std'] * np.random.randn(len(x))
         return x_new
 
-    def _update_proposal(self, x: np.ndarray, llh: float, log_p_acc: float,
+    def _update_proposal(self, x: np.ndarray, log_p_acc: float,
                          n_sample_cur: int):
         """Update the proposal density. Default: Do nothing."""
 
     def get_last_sample(self) -> InternalSample:
         return InternalSample(
             x=self.trace_x[-1],
-            llh=- self.trace_fval[-1]
+            lpost=- self.trace_fval[-1]
         )
 
     def set_last_sample(self, sample: InternalSample):
         self.trace_x[-1] = sample.x
-        self.trace_fval[-1] = - sample.llh
+        self.trace_fval[-1] = - sample.lpost
 
     def get_samples(self) -> McmcPtResult:
         result = McmcPtResult(
