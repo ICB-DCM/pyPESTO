486310a8a88bffdef15dce0928bea5543507f4fe
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2020-05-13 17:16:00 +0200

    geweke test

---
+++ pypesto/sampling/diagnostics.py
@@ -0,0 +1,30 @@
+from typing import Dict, List, Sequence, Union
+import numpy as np
+
+from ..result import Result
+from .result import McmcPtResult
+from .geweke_test import burnInBySequentialGeweke
+
+def GewekeTest(result: Result, zscore: float = 2.):
+    ''' Calculates the burn-in of MCMC chains.
+
+    Parameters
+    ----------
+    result:
+        The pyPESTO result object with filled sample result.
+    zscore:
+        The Geweke test threshold. Default 2.
+
+    Returns
+    -------
+    burn_in:
+        Iteration where the first and the last fraction of the chain
+        do not differ significantly regarding Geweke test -> Burn-In
+
+    '''
+    # get parameters and fval results as numpy arrays
+    chain = np.array(result.sample_result['trace_x'][0])
+
+    burn_in = burnInBySequentialGeweke(chain=chain, zscore=zscore)
+
+    return burn_in
