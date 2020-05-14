2c7955518b37142644e811b0e5362402c1ff58bc
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2020-05-14 11:46:20 +0200

    auto correlation added

--- pypesto/sampling/diagnostics.py
+++ pypesto/sampling/diagnostics.py
@@ -4,6 +4,7 @@ import numpy as np
 from ..result import Result
 from .result import McmcPtResult
 from .geweke_test import burnInBySequentialGeweke
+from .auto_correlation import auto_correlation
 
 def GewekeTest(result: Result, zscore: float = 2.):
     ''' Calculates the burn-in of MCMC chains.
@@ -29,3 +30,28 @@ def GewekeTest(result: Result, zscore: float = 2.):
     print('Geweke Burn-in index: '+str(burn_in))
 
     return burn_in
+
+def ChainAutoCorrelation(result: Result, burn_in: int = 0):
+    ''' Calculates the auto-correlation of the MCMC chains.
+
+    Parameters
+    ----------
+    result:
+        The pyPESTO result object with filled sample result.
+    burn_in:
+        The burn in index obtained from convergence tests,
+        e.g. Geweke. Default 0.
+
+    Returns
+    -------
+    tau:
+        Vector with estimated auto-correlation values.
+
+    '''
+    # get parameters and fval results as numpy arrays
+    # discarding warm up phase
+    chain = np.array(result.sample_result['trace_x'][0][burn_in:,:])
+
+    tau = auto_correlation(chain=chain)
+
+    return tau
