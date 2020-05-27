e8b307a7858b4076a4056a9b7127d529d114926b
lcontento <lcontento@users.noreply.github.com>
2020-05-27 18:51:44 +0200

    Cache gradients for pyMC3
    It seems that pyMC3 always calls the gradient after the log-posterior. By computing them together and using caching speed is improved.

--- pypesto/sampling/pymc3.py
+++ pypesto/sampling/pymc3.py
@@ -135,21 +135,21 @@ class TheanoLogProbability(tt.Op):
 
     def __init__(self, problem: Problem, beta: float = 1.):
         self._objective: Objective = problem.objective
-
-        # initialize the log probability Op
-        self._log_prob = \
-            lambda x: - beta * self._objective(x, sensi_orders=(0,))
-
+        self._beta: float = beta
+        self._cached_theta = None
+        self._cached_dlogp = None
         # initialize the sensitivity Op
         if problem.objective.has_grad:
-            self._log_prob_grad = TheanoLogProbabilityGradient(problem, beta)
+            self._log_prob_grad = TheanoLogProbabilityGradient(self)
         else:
             self._log_prob_grad = None
 
     def perform(self, node, inputs, outputs, params=None):
         theta, = inputs
-        log_prob = self._log_prob(theta)
-        outputs[0][0] = np.array(log_prob)
+        logp, dlogp = self._objective(theta, sensi_orders=(0, 1))
+        outputs[0][0] = np.array(-self._beta * logp)
+        self._cached_theta = theta
+        self._cached_dlogp = dlogp
 
     def grad(self, inputs, g):
         # the method that calculates the gradients - it actually returns the
@@ -167,25 +167,23 @@ class TheanoLogProbabilityGradient(tt.Op):
     Theano wrapper around a (non-normalized) log-probability gradient function.
     This Op will be called with a vector of values and also return a vector of
     values - the gradients in each dimension.
-
-    Parameters
-    ----------
-    problem:
-        The `pypesto.Problem` to analyze.
-    beta:
-        Inverse temperature (e.g. in parallel tempering).
     """
 
     itypes = [tt.dvector]  # expects a vector of parameter values when called
     otypes = [tt.dvector]  # outputs a vector (the log prob grad)
 
-    def __init__(self, problem: Problem, beta: float = 1.):
-        self._objective: Objective = problem.objective
-        self._log_prob_grad = \
-            lambda x: - beta * self._objective(x, sensi_orders=(1,))
+    def __init__(self, logp_op: TheanoLogProbability):
+        self._logp_op: TheanoLogProbability = logp_op
+        self._objective = logp_op._objective
+        self._beta = logp_op._beta
 
     def perform(self, node, inputs, outputs, params=None):
         theta, = inputs
-        # calculate gradients
-        log_prob_grad = self._log_prob_grad(theta)
-        outputs[0][0] = log_prob_grad
+
+        cached_theta = self._logp_op._cached_theta
+        if cached_theta is not None and np.all(theta == cached_theta):
+            dlogp = self._logp_op._cached_dlogp
+        else:
+            dlogp = self._objective(theta, sensi_orders=(1,))
+
+        outputs[0][0] = -self._beta * dlogp
