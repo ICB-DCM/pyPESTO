fix minus sign in get_error_output

pypesto/objective/amici.py
@@ -294,7 +294,8 @@ class AmiciObjective(ObjectiveBase):
 
         # check whether we should update data for preequilibration guesses
         if self.guess_steadystate and \
-                nllh <= self.steadystate_guesses['fval']:
+                nllh <= self.steadystate_guesses['fval'] and \
+                nllh < np.inf:
             self.steadystate_guesses['fval'] = nllh
             for data_ix, rdata in enumerate(rdatas):
                 self.store_steadystate_guess(data_ix, x_dct, rdata)

pypesto/objective/amici_util.py
@@ -234,7 +234,7 @@ def get_error_output(
     n_res = nt * amici_model.nytrue
 
     return {
-        FVAL: np.inf,
+        FVAL: - np.inf,
         GRAD: np.nan * np.ones(dim),
         HESS: np.nan * np.ones([dim, dim]),
         RES:  np.nan * np.ones(n_res),