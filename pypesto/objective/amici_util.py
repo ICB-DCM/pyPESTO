fix 2 signs

pypesto/objective/amici_util.py
@@ -225,7 +225,11 @@ def get_error_output(
         edatas: Sequence['amici.ExpData'],
         rdatas: Sequence['amici.ReturnData'],
         dim: int):
-    """Default output upon error."""
+    """Default output upon error.
+
+    Returns values indicative of an error, that is with nan entries in all
+    vectors, and a function value, i.e. nllh, of `np.inf`.
+    """
     if not amici_model.nt():
         nt = sum([data.nt() for data in edatas])
     else:
@@ -234,7 +238,7 @@ def get_error_output(
     n_res = nt * amici_model.nytrue
 
     return {
-        FVAL: - np.inf,
+        FVAL: np.inf,
         GRAD: np.nan * np.ones(dim),
         HESS: np.nan * np.ones([dim, dim]),
         RES:  np.nan * np.ones(n_res),