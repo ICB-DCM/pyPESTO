6793d8ee14dcba5a948151bb61b093115313664d
Jakob Vanhoefer <ja131va@yahoo.de>
2020-06-05 13:04:28 +0200

    bugfix

--- pypesto/problem.py
+++ pypesto/problem.py
@@ -175,7 +175,7 @@ class Problem:
             raise AssertionError("lb_full dimension invalid.")
         if self.ub_full.size != self.dim_full:
             raise AssertionError("ub_full dimension invalid.")
-        if self.x_scales.size != self.dim_full:
+        if len(self.x_scales) != self.dim_full:
             raise AssertionError("x_scales dimension invalid.")
         if check_x_guesses:
             if self.x_guesses.shape[1] != self.dim:
