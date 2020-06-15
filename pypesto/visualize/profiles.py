f3e08948ffa95c92916f98e05ef62810c7e78888
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2020-06-15 21:58:21 +0200

    hopefully fix profile_list and profile_index logic

--- pypesto/visualize/profiles.py
+++ pypesto/visualize/profiles.py
@@ -54,8 +54,6 @@ def profiles(results, ax=None, profile_indices=None, size=(18.5, 6.5),
     # parse input
     results, profile_list_ids, colors, legends = process_result_list_profiles(
         results, profile_list_ids, colors, legends)
-    (=, profile_list_ids, colors, legends) = process_result_list(
-        results, colors, legends)
 
     # get the parameter ids to be plotted
     profile_indices = process_profile_indices(results, profile_indices,
@@ -63,7 +61,7 @@ def profiles(results, ax=None, profile_indices=None, size=(18.5, 6.5),
 
     # loop over results
     for j, result in enumerate(results):
-        for profile_list_id in profile_list_ids:
+        for k, profile_list_id in enumerate(profile_list_ids):
             fvals = handle_inputs(result, profile_indices=profile_indices,
                                   profile_list=profile_list_id,
                                   ratio_min=ratio_min)
@@ -72,10 +70,16 @@ def profiles(results, ax=None, profile_indices=None, size=(18.5, 6.5),
                 x_labels = [name for name, fval in
                             zip(result.problem.x_names, fvals)
                             if fval is not None]
+
+            if len(results) == 1 and len(profile_list_ids) > 1:
+                color_ind = k
+            else:
+                color_ind = j
+
             # call lowlevel routine
             ax = profiles_lowlevel(
-                fvals=fvals, ax=ax, size=size, color=colors[j],
-                legend_text=legends[j], x_labels=x_labels,
+                fvals=fvals, ax=ax, size=size, color=colors[color_ind],
+                legend_text=legends[color_ind], x_labels=x_labels,
                 show_bounds=show_bounds, lb_full=result.problem.lb_full,
                 ub_full=result.problem.ub_full)
 
@@ -83,7 +87,7 @@ def profiles(results, ax=None, profile_indices=None, size=(18.5, 6.5),
     ref = create_references(references=reference)
 
     # plot reference points
-    ax = handle_reference_points(ref, ax, fvals)
+    ax = handle_reference_points(ref, ax, profile_indices)
 
     plt.tight_layout()
 
@@ -142,7 +146,7 @@ def profiles_lowlevel(
 
     # count number of necessary axes
     if isinstance(fvals, list):
-        n_fvals = np.sum([1 for fval in fvals if fval is not None])
+        n_fvals = len(fvals)
     else:
         n_fvals = 1
         fvals = [fvals]
@@ -150,10 +154,19 @@ def profiles_lowlevel(
     # number of non-trivial profiles
     n_profiles = sum((fval is not None for fval in fvals))
 
-    # if axes already exist: does the number of axes fit?
-    if n_profiles != len(ax) and not create_new_ax:
-        raise ValueError(
-            "Number of axes does not match number of profiles. Stopping.")
+    # if axes already exists, we have to match profiles to axes
+    if not create_new_ax:
+        if n_fvals != len(ax) and n_profiles != len(ax):
+            raise ValueError(
+                "Number of axes does not match number of profiles. Stopping.")
+        elif n_fvals == len(ax) and n_profiles != len(ax):
+            # we may have some empty profiles, which we have to skip
+            n_plots = n_fvals
+        else:
+            # n_profiles == len(ax):, we have exactly as many profiles as axes
+            n_plots = n_profiles
+    else:
+        n_plots = n_profiles
 
     if lb_full is None:
         lb_full = [None] * len(fvals)
@@ -161,45 +174,50 @@ def profiles_lowlevel(
         ub_full = [None] * len(fvals)
 
     # compute number of columns and rows
-    columns = np.ceil(np.sqrt(n_fvals))
-    if n_fvals > columns * (columns - 1):
+    columns = np.ceil(np.sqrt(n_plots))
+    if n_plots > columns * (columns - 1):
         rows = columns
     else:
         rows = columns - 1
 
     counter = 0
     for i_plot, (fval, lb, ub) in enumerate(zip(fvals, lb_full, ub_full)):
+        # if we have empty profiles and more axes than profiles: skip
+        if n_plots != n_fvals and fval is None:
+            continue
+
         # handle legend
         if i_plot == 0:
             tmp_legend = legend_text
         else:
             tmp_legend = None
+
+        # create or choose an axes object
+        if create_new_ax:
+            ax.append(fig.add_subplot(rows, columns, counter + 1))
+        else:
+            plt.axes(ax[counter])
+
         # plot if data
         if fval is not None:
-            # create or choose an axes object
-            if create_new_ax:
-                ax.append(fig.add_subplot(rows, columns, counter + 1))
-            else:
-                plt.axes(ax[counter])
-
             # run lowlevel routine for one profile
             ax[counter] = profile_lowlevel(
                 fval, ax[counter], size=size, color=color,
                 legend_text=tmp_legend, show_bounds=show_bounds, lb=lb, ub=ub)
 
-            # labels
-            if x_labels is None:
-                ax[counter].set_xlabel(f'Parameter {i_plot}')
-            else:
-                ax[counter].set_xlabel(x_labels[counter])
+        # labels
+        if x_labels is None:
+            ax[counter].set_xlabel(f'Parameter {i_plot}')
+        else:
+            ax[counter].set_xlabel(x_labels[counter])
 
-            if counter % columns == 0:
-                ax[counter].set_ylabel('Log-posterior ratio')
-            else:
-                ax[counter].set_yticklabels([''])
+        if counter % columns == 0:
+            ax[counter].set_ylabel('Log-posterior ratio')
+        else:
+            ax[counter].set_yticklabels([''])
 
-            # increase counter and cleanup legend
-            counter += 1
+        # increase counter and cleanup legend
+        counter += 1
 
     return ax
 
@@ -265,7 +283,7 @@ def profile_lowlevel(
     return ax
 
 
-def handle_reference_points(ref, ax, fvals):
+def handle_reference_points(ref, ax, profile_indices):
     """
     Handle reference points.
 
@@ -276,16 +294,15 @@ def handle_reference_points(ref, ax, fvals):
         least a function value fval
     ax: matplotlib.Axes, optional
         Axes object to use.
-    fvals: numeric list or array
-        Values to plot.
+    profile_indices: list of integer values
+        List of integer values specifying which profiles should be plotted.
     """
 
     if len(ref) > 0:
         # get the parameters which have profiles plotted
         par_indices = []
-        for i_plot, fval in enumerate(fvals):
-            if fval is not None:
-                par_indices.append(i_plot)
+        for i_plot, profile_ind in enumerate(profile_indices):
+            par_indices.append(profile_ind)
 
         # loop over axes objects
         for i_par, i_ax in enumerate(ax):
@@ -377,27 +394,25 @@ def process_result_list_profiles(results, profile_list_ids, colors, legends):
         labels for line plots
     """
 
-    # check if we have a single result
-    single_result = False
-    if isinstance(results, list):
-        if len(results) == 1:
-            single_result = True
-    else:
-        single_result = True
-        results = [results]
-
     # ensure list of ids
     if isinstance(profile_list_ids, int):
         profile_list_ids = [profile_list_ids]
 
-    # If we have a single result, we may still have multiple profile_list_ids.
-    # Hence, multiple colors and legends would be fine.
-    if single_result:
-        pass
+    # check if we have a single result
+    if isinstance(results, list):
+        if len(results) != 1:
+            # if we have no single result, then use the standard api
+            results, colors, legends = process_result_list(
+                results, colors, legends)
+            return results, profile_list_ids, colors, legends
     else:
-        # if we have no single result, then use the standard api
-        results, colors, legends = process_result_list(
-            results, colors, legends)
+        # a single results was provided, so make a list out of it
+        results = [results]
+
+    # If we have a single result, we may still have multiple profile_list_ids
+    # which should be plotted separately: use profile_list_ids as results dummy
+    _, colors, legends = process_result_list(
+            profile_list_ids, colors, legends)
 
     return results, profile_list_ids, colors, legends
 
