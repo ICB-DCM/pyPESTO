31e99ea2ee4ca36d4d412f93a00f72497c924ded
dilpath <dilan.private+github@outlook.com>
2020-06-15 23:22:48 +0200

    Refactor, new function.

--- pypesto/logging.py
+++ pypesto/logging.py
@@ -7,67 +7,63 @@ Logging convenience functions.
 
 import logging
 
+def log(name: str = 'pypesto',
+        level: int = None,
+        console: bool = False,
+        filename: str = ''):
+    '''
+    Log messages from a specified name with a specified level to any
+    combination of console and file.
 
-def log_to_console(level: int = None, child: str = None):
+    Parameters
+    ----------
+    name:
+        The name of the logger.
+
+    level:
+        The output level to use. Default: logging.DEBUG.
+
+    console:
+        If True, messages are logged to console.
+
+    filename:
+        If specified, messages are logged to a file with this name.
+    '''
+    if level is None:
+        level = logging.DEBUG
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    if console:
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        logger.addHandler(ch)
+
+    if filename:
+        fh = logging.FileHandler(filename)
+        fh.setLevel(level)
+        logger.addHandler(fh)
+
+def log_to_console(level: int = None):
     """
     Log to console.
 
     Parameters
     ----------
-
-    level:
-        The output level to use. Default: logging.DEBUG.
-
-    child:
-        The name of the descendant to the 'pypesto' logger. For example, if
-        child is 'model_selection', then the logger name will be
-        'pypesto.model_selection'.
-
+    See the `log` method.
     """
-    if level is None:
-        level = logging.DEBUG
-
-    logger = logging.getLogger('pypesto')
-    if child is not None:
-        logger = logger.getChild(child)
-
-    logger.setLevel(level)
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
-    logger.addHandler(ch)
+    log(level=level, console=True)
 
 
-def log_to_file(level: int = None, filename: str = None, child: str = None):
+def log_to_file(level: int = None, filename: str = None):
     """
     Log to file.
 
     Parameters
     ----------
-
-    level:
-        The output level to use. Default: logging.DEBUG.
-
-    filename:
-        The name of the file to append to.
-        Default: .pypesto_logging.log.
-
-    child:
-        The name of the descendant to the 'pypesto' logger. For example, if
-        child is 'model_selection', then the logger name will be
-        'pypesto.model_selection'.
+    See the `log` method.
     """
-
-    if level is None:
-        level = logging.DEBUG
-
     if filename is None:
         filename = ".pypesto_logging.log"
-
-    logger = logging.getLogger('pypesto')
-    if child is not None:
-        logger = logger.getChild(child)
-
-    logger.setLevel(level)
-    fh = logging.FileHandler(filename)
-    fh.setLevel(level)
-    logger.addHandler(fh)
+    log(level=level, filename=filename)
