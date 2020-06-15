15e269b53d4489fe9df73ea35d98e9921c19b531
dilpath <dilan.private+github@outlook.com>
2020-06-15 20:33:25 +0200

    Specify logger descendant.

--- pypesto/logging.py
+++ pypesto/logging.py
@@ -8,40 +8,53 @@ Logging convenience functions.
 import logging
 
 
-def log_to_console(level=None):
+def log_to_console(level: int = None, child: str = None):
     """
     Log to console.
 
     Parameters
     ----------
 
-    level: int
+    level:
         The output level to use. Default: logging.DEBUG.
+
+    child:
+        The name of the descendant to the 'pypesto' logger. For example, if
+        child is 'model_selection', then the logger name will be
+        'pypesto.model_selection'.
 
     """
     if level is None:
         level = logging.DEBUG
 
     logger = logging.getLogger('pypesto')
+    if child is not None:
+        logger = logger.getChild(child)
+
     logger.setLevel(level)
     ch = logging.StreamHandler()
     ch.setLevel(level)
     logger.addHandler(ch)
 
 
-def log_to_file(level=None, filename=None):
+def log_to_file(level: int = None, filename: str = None, child: str = None):
     """
     Log to file.
 
     Parameters
     ----------
 
-    level: int
+    level:
         The output level to use. Default: logging.DEBUG.
 
-    filename: str
+    filename:
         The name of the file to append to.
         Default: .pypesto_logging.log.
+
+    child:
+        The name of the descendant to the 'pypesto' logger. For example, if
+        child is 'model_selection', then the logger name will be
+        'pypesto.model_selection'.
     """
 
     if level is None:
@@ -51,6 +64,9 @@ def log_to_file(level=None, filename=None):
         filename = ".pypesto_logging.log"
 
     logger = logging.getLogger('pypesto')
+    if child is not None:
+        logger = logger.getChild(child)
+
     logger.setLevel(level)
     fh = logging.FileHandler(filename)
     fh.setLevel(level)
