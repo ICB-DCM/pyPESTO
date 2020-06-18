03f5671d281b6b1203c6c857356ba25688f39821
yannikschaelte <yannik.schaelte@gmail.com>
2020-06-18 17:45:59 +0200

    fixup

--- test/check_git_size.sh
+++ test/check_git_size.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-size=$(du -s .git | cut -f1)
+# Check .git log size
+# Over time, this will increase s.t. the value will need to be adjusted.
+# However, if jumps in the size occur, this may indicate that undesirably
+# large files have been included.
 
-if [ "$size" -gt 20000 ]; then
-  echo "Git history is suspiciously large: $size"
-  exit 1
+size=$(du -s .git | cut -f1);
+
+if [ "$size" -gt 50000 ]; then
+  echo "Git history is suspiciously large: $size";
+  exit 1;
 fi
