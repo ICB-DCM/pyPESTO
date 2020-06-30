001345d86822686bfe44a2c0f8510cbc6ed06faa
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2020-06-30 17:18:38 +0200

    docs

--- pypesto/sample/auto_correlation.py
+++ pypesto/sample/auto_correlation.py
@@ -1,30 +1,18 @@
-import logging
 import numpy as np
 
 
 def autocorrelation_sokal(chain):
-    """Estimate the integrated autocorrelation time of a time series.
-    This estimate uses the iterative procedure described on page 16 of
-    `Sokal's notes <http://www.stat.unc.edu/faculty/cji/Sokal.pdf>`_ to
-    determine a reasonable window size.
-    Args:
-        x: The time series. If multidimensional, set the time axis using the
-            ``axis`` keyword argument and the function will be computed for
-            every other axis.
-        c (Optional[float]): The step size for the window search. (default:
-            ``5``)
-        tol (Optional[float]): The minimum number of autocorrelation times
-            needed to trust the estimate. (default: ``50``)
-        quiet (Optional[bool]): This argument controls the behavior when the
-            chain is too short. If ``True``, give a warning instead of raising
-            an :class:`AutocorrError`. (default: ``False``)
-    Returns:
-        float or array: An estimate of the integrated autocorrelation time of
-            the time series ``x`` computed along the axis ``axis``.
-    Raises
-        AutocorrError: If the autocorrelation time can't be reliably estimated
-            from the chain and ``quiet`` is ``False``. This normally means
-            that the chain is too short.
+    """
+    Estimate the integrated autocorrelation time of a MCMC chain
+    using Sokal's adaptive truncated periodogram estimator.
+
+    Parameters
+    ----------
+        chain: The MCMC chain.
+    Returns
+    -------
+        tau_est: An estimate of the integrated autocorrelation time of
+        the MCMC chain.
     """
 
     mx, nx = chain.shape
