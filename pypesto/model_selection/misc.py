654cd91d527645b043ba2123659e80aeb7fd61bb
dilpath <dilan.private+github@outlook.com>
2020-08-08 14:34:00 +0200

    Fix AIC/BIC docstrings.

--- pypesto/model_selection/misc.py
+++ pypesto/model_selection/misc.py
@@ -141,8 +141,9 @@ def aic(n_estimated: int, nllh: float) -> float:
         The number of estimated parameters in the model.
 
     nllh:
-        The negative log likelihood (e.g. the `optimize_result.list[0].fval`
-        attribute of the return value of `pypesto.minimize`).
+        The negative log likelihood,
+        e.g.: the `optimize_result.list[0]['fval']` attribute of the object
+        returned by `pypesto.minimize`.
     """
     return 2*(n_estimated + nllh)
 
@@ -157,13 +158,20 @@ def bic(n_estimated: int, nllh: float, n_measurements: int, n_priors: int):
     n_estimated:
         The number of estimated parameters in the model.
 
-    n_observations:
-        The number of measurements used in the objective function of the model
-        (e.g. `len(petab_problem.measurement_df)`).
-
     nllh:
-        The negative log likelihood (e.g. the `optimize_result.list[0].fval`
-        attribute of the return value of `pypesto.minimize`).
+        The negative log likelihood,
+        e.g.: the `optimize_result.list[0]['fval']` attribute of the object
+        returned by `pypesto.minimize`.
+
+    n_measurements:
+        The number of measurements used in the objective function of the model,
+        e.g.: `len(petab_problem.measurement_df)`.
+
+    n_priors:
+        The number of priors used in the objective function of the model,
+        e.g.: `len(pypesto_problem.x_priors._objectives)`.
+        TODO make public property for number of priors in objective? or in
+             problem, since `x_priors` == None is possible.
     """
     return n_estimated*math.log(n_measurements + n_priors) + 2*nllh
 
