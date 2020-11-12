05d8d82170884f987b1c6f423e52d3c61304da7b
LarsFroehling <larsfroe@gmail.com>
2020-11-12 16:29:15 +0100

    refactor, small changes

--- pypesto/optdesign/change_dataframe.py
+++ pypesto/optdesign/change_dataframe.py
@@ -1,14 +1,17 @@
 import numpy as np
-from .result import DesignResult
+# from .result import DesignResult
+from .opt_design_helpers import get_design_result
 from .design_problem import DesignProblem
 import amici
-from typing import Iterable
+from typing import Iterable, List
 
 
-def get_fim_addition(design_result: DesignResult,
-                     candidate,
-                     deriv_dict,
-                     dict_of_timepoints_cond):
+# TODO rename this file to something like "FIM_computations.py" or similar
+
+def get_fim_addition(design_problem: DesignProblem,
+                     candidate: dict,
+                     deriv_dict: dict,
+                     dict_of_timepoints_cond: dict):
     """
     uses the previously computed parameter derivatives of the observables
     and computes the effect of a candidate on the FIM
@@ -19,9 +22,9 @@ def get_fim_addition(design_result: DesignResult,
 
     Parameters
     ----------
-    design_result: the optimal design result
-    candidate: measurement_df, (condition_df, observable_df) with candidate
-               measurements that we want to check
+    design_problem: the optimal design problem
+    candidate: dict with an id, measurement_df, (condition_df, observable_df)
+    with candidate measurements that we want to check
     deriv_dict: a dictionary, contains for each experimental condition the
               parameter derivatives of the observables
     dict_of_timepoints_cond: a dictionary with the time points that we are
@@ -32,7 +35,8 @@ def get_fim_addition(design_result: DesignResult,
     A: matrix A such that A*A^T is the total addition to the FIM
     """
 
-    design_problem = design_result.design_problem
+    model = design_problem.problem.objective.amici_model
+
     A = np.nan * np.ones((len(design_problem.initial_x[0]),
                           len(candidate['measurement_df'])))
     for row_index in range(len(candidate['measurement_df'])):
@@ -41,16 +45,19 @@ def get_fim_addition(design_result: DesignResult,
         deriv_cond = deriv_dict[cond_name]
 
         # ie fixed noise parameters
-        missing_params = len(design_problem.model.getParameterIds()) - len(
+        missing_params = len(model.getParameterIds()) - len(
             design_problem.initial_x[0])
 
-        obs_position = design_problem.model.getObservableIds().index(
+        obs_position = model.getObservableIds().index(
             candidate['measurement_df'].observableId[row_index])
         time_position = dict_of_timepoints_cond[cond_name].index(
             candidate['measurement_df'].time[row_index])
-        jac = deriv_cond[time_position][0:len(deriv_cond[0]) -
-                                          missing_params, obs_position]
+        jac = deriv_cond[time_position][0:len(deriv_cond[0]) - missing_params,
+              obs_position]
 
+        # in design_problem.number_of_measurements one can specify how many
+        # measurements should be taken, this effectively reduces the noise
+        # value
         A[:, row_index] = jac * np.sqrt(
             design_problem.number_of_measurements) \
                           / candidate['measurement_df'].noiseParameters[
@@ -62,7 +69,6 @@ def get_fim_addition(design_result: DesignResult,
         # total_addition = total_addition + (
         #         design_problem.number_of_measurements *
         #         single_addition)
-    # total_addition = np.matmul(A, A.transpose())
     return A
 
 
@@ -85,14 +91,12 @@ def get_derivatives(design_problem: DesignProblem,
     each condition
     """
     deriv_dict = {}
-
-    model = design_problem.model
+    model = design_problem.problem.objective.amici_model
     solver = model.getSolver()
     solver.setSensitivityOrder(amici.SensitivityOrder_first)
     model.setParameterScale(amici.ParameterScaling_log10)
     temp = x
 
-    # solver.setMaxSteps(1000000)
     # these may be noise parameters for which we have explicit values
     missing_params = len(model.getParameterIds()) - len(temp)
     for i in range(missing_params):
@@ -118,3 +122,55 @@ def get_derivatives(design_problem: DesignProblem,
 
         deriv_dict[condition] = vec
     return deriv_dict
+
+
+def get_combi_run_result(relevant_single_runs: List[dict],
+                         initial_result: dict,
+                         design_problem: DesignProblem,
+                         combi: list,
+                         x: Iterable) \
+        -> List[dict]:
+    """
+    computes the new criteria values etc in a dict after adding the new
+    addition to the FIM
+
+    Parameters
+    ----------
+    relevant_single_runs: the result of the candidates specified in the
+    experiment list. 'relevant' because only the ones for this particular x
+    initial_result: the result without adding any new measurements for this x
+    design_problem: the problem formulation
+    combi: a list of indices specifying which candidates from
+    design_problem.experiment_list should be combined
+    x: current set of parameters used
+
+    Returns
+    -------
+    result: a dict with info about the combination specified in 'combi'
+            saves in particular criteria values
+    """
+    # we will consider FIM_new = FIM_old + v*v^T + w*w^T + x*x^T + ...
+    # where v,w,x are vectors and '*' is the outer product
+    # one can write v*v^T + w*w^T + x*x^T + ... = M*M^T where M is the matrix
+    # with v,w,x,... as columns
+    # here we create this Matrix as 'total_hess_additional', and use it in
+    # 'get_design_result' to compute the new FIM + criteria
+
+    total_hess_additional = relevant_single_runs[combi[0]]['hess_additional']
+    for index in combi[1:]:
+        total_hess_additional = np.hstack((total_hess_additional,
+                                           relevant_single_runs[index][
+                                               'hess_additional']))
+
+    # TODO skip, dict_min etc for faster criteria computations have to be
+    #  handled here
+    result = get_design_result(
+        design_problem=design_problem,
+        candidate=combi,
+        x=x,
+        hess=initial_result['hess'],
+        hess_additional=total_hess_additional,
+        initial_result=initial_result,
+        skip=True)
+
+    return result
