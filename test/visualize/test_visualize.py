a3eb6e6f0c07e35fb2014b53f99fc4f008385a16
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2020-11-17 15:49:22 +0100

    fix

--- test/visualize/test_visualize.py
+++ test/visualize/test_visualize.py
@@ -47,12 +47,14 @@ def create_problem():
 
 
 def create_petab_problem():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.path.abspath(os.path.join(current_path,
+                                            '..', '..',
+                                            'doc', 'example'))
 
     # import to petab
     petab_problem = petab.Problem.from_yaml(
-        dir_path.rsplit('/', 1)[0] +
-        "/conversion_reaction/conversion_reaction.yaml")
+        dir_path + "/conversion_reaction/conversion_reaction.yaml")
     # import to pypesto
     importer = pypesto.petab.PetabImporter(petab_problem)
     # create problem
