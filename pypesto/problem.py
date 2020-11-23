506c5b4f168892857fd4ec2feda6e98915eb2c37
Jakob Vanhoefer <38346459+jvanhoefer@users.noreply.github.com>
2020-11-23 16:13:14 +0100

    Update pypesto/problem.py
    Co-authored-by: Fabian Fröhlich <fabian@schaluck.com>

--- pypesto/problem.py
+++ pypesto/problem.py
@@ -117,7 +117,6 @@ class Problem:
 
         self._x_free_indices: Union[List[int], None] = None
 
-        # check if lower bounds are not nan
 
         if x_guesses is None:
             x_guesses = np.zeros((0, self.dim_full))
