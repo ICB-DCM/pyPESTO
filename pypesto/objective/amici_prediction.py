fdac30dcca302b26d38253877dc4d51fe3825344
paulstapor <paul.stapor@helmholtz-muenchen.de>
2020-12-16 10:49:03 +0100

    encapsulate call to amici objective to enable slicing of calls to amici objective

--- pypesto/objective/amici_prediction.py
+++ pypesto/objective/amici_prediction.py
@@ -31,7 +31,7 @@ class AmiciPrediction():
                  amici_objective: AmiciObjective,
                  post_processing: Union[Callable, None] = None,
                  post_processing_sensi: Union[Callable, None] = None,
-                 max_num_conditions: int = 0):
+                 max_num_conditions: int = int('inf')):
         """
         Constructor.
 
@@ -52,7 +52,7 @@ class AmiciPrediction():
             the memory for large datasets and models.
             Here, the user can specify a maximum number of conditions, which
             should be simulated at a time.
-            Default is 0 meaning that all conditions will be simulated.
+            Default is 'inf' meaning that all conditions will be simulated.
             Other values are only applicable, if an output file is specified.
         """
         # save settings
@@ -109,49 +109,76 @@ class AmiciPrediction():
             raise Exception('Prediction simulation does currently not support '
                             'second order output.')
 
+        # prepare for storing the results
+        outputs = []
+        outputs_sensi = []
+        timepoints = []
+
         # simulate the model and get the output
-        ret = self.amici_objective(x=x, sensi_orders=sensi_orders,
-                                   mode=mode, return_dict=True)
-        rdatas = ret['rdatas']
+        self._get_model_outputs(outputs, outputs_sensi, timepoints,
+                                x, sensi_orders, mode)
 
-        # post process
-        output = []
-        output_sensi = []
-        if 0 in sensi_orders:
-            output = self.post_processing(rdatas)
-        if 1 in sensi_orders:
-            output_sensi = self.post_processing_sensi(rdatas)
-
-        if output_file == '':
-            if sensi_orders == (0, 1):
-                return output, output_sensi
-            elif sensi_orders == (0,):
-                return output
-            elif sensi_orders == (1,):
-                return output_sensi
-            else:
-                raise Exception('Prediction simulation called with unsupported '
-                                'input for sensi_orders:', sensi_orders)
-        else:
+        if output_file != '':
             if output_format == 'csv':
-                self.write_to_csv(outputs=output,
-                                  outputs_sensi=output_sensi,
-                                  output_file=output_file,
-                                  rdatas=rdatas)
+                self._write_to_csv(outputs=outputs,
+                                   outputs_sensi=outputs_sensi,
+                                   output_file=output_file,
+                                   timepoints=timepoints)
             elif output_format == 'h5':
-                self.write_to_h5(outputs=output,
-                                 outputs_sensi=output_sensi,
-                                 output_file=output_file,
-                                 rdatas=rdatas)
+                self._write_to_h5(outputs=outputs,
+                                  outputs_sensi=outputs_sensi,
+                                  output_file=output_file,
+                                  timepoints=timepoints)
             else:
                 raise Exception(f'Call to unknown format {output_format} for '
                                 f'output of pyPESTO prediction.')
 
-    def write_to_csv(self,
-                     outputs: Sequence[np.ndarray],
-                     outputs_sensi: Sequence[np.ndarray],
-                     output_file: str,
-                     rdatas: Sequence):
+        # return dependent on sensitivity order
+        if sensi_orders == (0, 1):
+            return outputs, outputs_sensi
+        elif sensi_orders == (0,):
+            return outputs
+        elif sensi_orders == (1,):
+            return outputs_sensi
+        else:
+            raise Exception('Prediction simulation called with unsupported '
+                            'input for sensi_orders:', sensi_orders)
+
+    def _get_model_outputs(self,
+                           outputs,
+                           outputs_sensi,
+                           timepoints,
+                           x,
+                           sensi_orders,
+                           mode):
+        """
+        The main purpose of this function is to encapsulate the call to amici:
+        This allows to use variable scoping as a mean to clean up the memory
+        after calling amici, which is beneficial if large models with large
+        datasets are used
+        """
+
+        n_simulations = 1 + int(len(self.amici_objective.edatas) /
+                                self.max_num_conditions)
+        for i_sim in range(n_simulations):
+            ids = slice(i_sim * self.max_num_conditions,
+                        (i_sim + 1) * self.max_num_conditions)
+
+            ret = self.amici_objective(x=x, sensi_orders=sensi_orders,
+                                       edatas=self.amici_objective.edatas[ids],
+                                       mode=mode, return_dict=True)
+            # post process
+            timepoints += [rdata['t'] for rdata in ret['rdatas']]
+            if 0 in sensi_orders:
+                outputs += self.post_processing(ret['rdatas'])
+            if 1 in sensi_orders:
+                outputs_sensi += self.post_processing_sensi(ret['rdatas'])
+
+    def _write_to_csv(self,
+                      outputs: Sequence[np.ndarray],
+                      outputs_sensi: Sequence[np.ndarray],
+                      output_file: str,
+                      timepoints: Sequence):
         """
         This method saves predictions from an amici model to a given file.
         """
@@ -188,28 +215,28 @@ class AmiciPrediction():
                 tmp_filename = os.path.join(output_path,
                     output_file_dummy + f'_{i_out}.' + output_file_suffix)
 
-                timepoints = rdatas[i_out]['t']
-                result = pd.DataFrame(index=timepoints, columns=observables,
+                i_timepoints = timepoints[i_out]
+                result = pd.DataFrame(index=i_timepoints, columns=observables,
                                       data=output)
                 result.to_csv(tmp_filename, sep='\t')
 
         if outputs_sensi:
             for i_out, output_sensi in enumerate(outputs_sensi):
-                timepoints = rdatas[i_out]['t']
-                for i_par in range(rdatas[i_out]['sx'].shape[2]):
+                i_timepoints = timepoints[i_out]
+                for i_par in range(output_sensi[i_out].shape[1]):
                     tmp_filename = os.path.join(output_path, output_file_dummy +
                                                 f'_{i_out}__s{i_par}.' +
                                                 output_file_suffix)
-                    result = pd.DataFrame(index=timepoints,
+                    result = pd.DataFrame(index=i_timepoints,
                                           columns=observables,
                                           data=output_sensi[:,i_par,:])
                     result.to_csv(tmp_filename, sep='\t')
 
-    def write_to_h5(self,
-                    outputs: Sequence[np.ndarray],
-                    outputs_sensi: Sequence[np.ndarray],
-                    output_file: str,
-                    rdatas: Sequence):
+    def _write_to_h5(self,
+                     outputs: Sequence[np.ndarray],
+                     outputs_sensi: Sequence[np.ndarray],
+                     output_file: str,
+                     timepoints: Sequence):
         """
         This method saves predictions from an amici model to a given file.
         """
@@ -221,13 +248,12 @@ class AmiciPrediction():
             n_groups = max(len(outputs), len(outputs_sensi))
             for i_out in range(n_groups):
                 f.create_group(str(i_out))
-                f.create_dataset(f'{i_out}/timepoints', data=rdatas[i_out]['t'])
+                f.create_dataset(f'{i_out}/timepoints', data=timepoints[i_out])
                 if outputs:
                     f.create_dataset(f'{i_out}/sim', data=outputs[i_out])
                 if outputs_sensi:
                     f.create_dataset(f'{i_out}/sim_sensi',
                                      data=outputs_sensi[i_out])
-
 
     @staticmethod
     def default_post_processing(rdatas) -> Sequence[np.ndarray]:
