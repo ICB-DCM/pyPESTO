3a982fc70b2a773ab7f67213ab74976034b6fca9
paulstapor <paul.stapor@helmholtz-muenchen.de>
2021-01-27 17:00:14 +0100

    add dimension reduction functionality via umap and pca

---
+++ pypesto/ensemble/covariance_analysis.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from typing import Sequence, Tuple, Callable, Dict
+
+from ..prediction import PredictionResult, PredictionConditionResult
+from .constants import (PREDICTOR, PREDICTION_ID, PREDICTION_RESULTS,
+                        PREDICTION_ARRAYS, PREDICTION_SUMMARY, OUTPUT,
+                        OUTPUT_SENSI, TIMEPOINTS, X_VECTOR, NX, X_NAMES,
+                        NVECTORS, VECTOR_TAGS, PREDICTIONS, MODE_FUN,
+                        EnsembleType, ENSEMBLE_TYPE, MEAN, MEDIAN,
+                        STANDARD_DEVIATION, PERCENTILE, SUMMARY, LOWER_BOUND,
+                        UPPER_BOUND)
+
+
+def get_covariance_matrix_parameters():
+    pass
+
+def get_covariance_matrix_predictions():
+    pass
+
+def get_spectral_decomposition_parameters():
+    pass
+
+def get_spectral_decomposition_predictions():
+    pass
+
+def _get_covariance_matrix_lowlevel():
+    pass
+
+def _get_spectral_decomposition_lowlevel():
+    pass
