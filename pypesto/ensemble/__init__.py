4691ad6b022a0aad3b6f766ad3c14baaa65d3c92
paulstapor <paul.stapor@helmholtz-muenchen.de>
2021-01-27 17:06:02 +0100

    make function usable from the outside

--- pypesto/ensemble/__init__.py
+++ pypesto/ensemble/__init__.py
@@ -5,3 +5,7 @@ Ensemble
 
 
 from .ensemble import Ensemble, EnsemblePrediction, read_from_csv
+from .dimension_reduction import (get_umap_representation_parameters,
+                                  get_umap_representation_predictions,
+                                  get_pca_representation_parameters,
+                                  get_pca_representation_predictions)
