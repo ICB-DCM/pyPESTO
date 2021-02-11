f64dfaadb7a59e110c66674c1f4f37bf50866a25
PaulJonasJost <jost.pauljonas@gmail.com>
2021-02-11 13:42:53 +0100

    added example and a test

---
+++ doc/example/example_MPIPool.py
@@ -0,0 +1,83 @@
+import pypesto
+import pypesto.visualize as visualize
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import pypesto.profile as profile
+import pypesto.optimize as optimize
+import time
+import timeit
+from datetime import timedelta
+# you need to manually import the MPIPoolEninge
+from pypesto.engine.mpi_pool import MPIPoolEngine
+# the below is needed for testing purposes.
+from numpy.testing import assert_almost_equal
+
+# set all your code into this if condition.
+# This way only one core performs the code
+# and distributes the work of the optimization.
+if __name__ == '__main__':
+    # first type of objective
+    objective1 = pypesto.Objective(fun=sp.optimize.rosen,
+                                   grad=sp.optimize.rosen_der,
+                                   hess=sp.optimize.rosen_hess)
+
+    dim_full = 10
+    lb = -5 * np.ones((dim_full, 1))
+    ub = 5 * np.ones((dim_full, 1))
+
+    # set number of starts
+    n_starts = 2
+
+    # fixing startpoints
+    startpoints = pypesto.startpoint.latin_hypercube(n_starts=n_starts,
+                                                     lb=lb,
+                                                     ub=ub)
+    problem1 = pypesto.Problem(objective=objective1, lb=lb, ub=ub,
+                               x_guesses=startpoints)
+
+    x = np.arange(-2, 2, 0.1)
+    y = np.arange(-2, 2, 0.1)
+    x, y = np.meshgrid(x, y)
+    z = np.zeros_like(x)
+    for j in range(0, x.shape[0]):
+        for k in range(0, x.shape[1]):
+            z[j,k] = objective1([x[j,k], y[j,k]], (0,))
+
+
+
+    # create different optimizers
+    optimizer = optimize.FidesOptimizer()
+
+    # save optimizer trace
+    history_options = pypesto.HistoryOptions(trace_record=True)
+
+    # result2 is the way to call the optimization with MPIPoolEngine.
+    result1 = optimize.minimize(
+            problem=problem1, optimizer=optimizer,
+            n_starts=n_starts, engine=pypesto.engine.MultiProcessEngine())
+    print('\n done with MultiProcessEngine \n')
+    result2 = optimize.minimize(
+            problem=problem1, optimizer=optimizer,
+            n_starts=n_starts, engine=MPIPoolEngine())
+    print('i am done with optimization')
+
+    # starting here are the tests (not needed in your code)
+    if(result1.optimize_result.list[0]['id'] == result2.optimize_result.list[0]['id']):
+        assert_almost_equal(result1.optimize_result.list[0]['x'], result2.optimize_result.list[0]['x'],
+                            err_msg='The final parameter values do not agree for the engines.')
+        assert_almost_equal(result1.optimize_result.list[1]['x'], result2.optimize_result.list[1]['x'],
+                            err_msg='The final parameter values do not agree for the engines.')
+    else:
+        assert_almost_equal(result1.optimize_result.list[0]['x'], result2.optimize_result.list[1]['x'],
+                            err_msg='The final parameter values do not agree for the engines.')
+        assert_almost_equal(result1.optimize_result.list[1]['x'], result2.optimize_result.list[0]['x'],
+                            err_msg='The final parameter values do not agree for the engines.')
+    print('also done with tests')
+
+
+
+
+
+    
--- test/optimize/test_optimize.py
+++ test/optimize/test_optimize.py
@@ -9,6 +9,7 @@ import re
 import nlopt
 import fides
 import itertools as itt
+import os
 
 import pypesto
 import pypesto.optimize as optimize
@@ -199,3 +200,12 @@ def check_minimize(objective, library, solver, allow_failed_starts=False):
     ]:
         assert np.isfinite(result.optimize_result.list[0]['fval'])
         assert result.optimize_result.list[0]['x'] is not None
+
+def test_MPIPool_Engine():
+    """
+    Test the MPIPoolEngine by calling an example script with mpiexec.
+    """
+    #get the path to this file:
+    path = os.path.dirname(__file__)
+    #run the example file.
+    os.system(f"mpiexec -np 2 python -m mpi4py.futures {path}../../doc/example/example_MPIPool.py")
