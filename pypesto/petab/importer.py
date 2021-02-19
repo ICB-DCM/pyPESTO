9da15baf041a62b082e73186e168caf2a09566a8
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2021-02-19 21:36:24 +0100

    Update pypesto/petab/importer.py
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>

--- pypesto/petab/importer.py
+++ pypesto/petab/importer.py
@@ -287,8 +287,8 @@ class PetabImporter(AmiciObjectBuilder):
         objective:
             An objective object, which will be used to get model simulations
         amici_output_fields:
-            keys which exist in the return data object from Amici, which should
-            be available for the post_processor routines
+            keys that exist in the return data object from AMICI, which should
+            be available for the post-processors
         post_processor:
             A callable function which applies postprocessing to the simulation
             results. Default are the observables of the amici model.
