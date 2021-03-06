09071a4db092afcab7b2731cc891be006fd93c96
yannikschaelte <yannik.schaelte@gmail.com>
2021-03-06 16:11:48 +0100

    fix tests

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -25,7 +25,9 @@ for par in "$@"; do
       # for amici
       sudo apt-get install \
         swig3.0 libatlas-base-dev libhdf5-serial-dev
-      sudo ln -s /usr/bin/Swig3.0 /usr/bin/swig
+      if [ ! -e /usr/bin/swig ]; then
+        sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
+      fi
     ;;
 
     ipopt)
