235e2c5a7b72bc24ae5270aa9460f7e9e885894a
yannikschaelte <yannik.schaelte@gmail.com>
2021-03-18 10:26:35 +0100

    test

--- pypesto/petab/__init__.py
+++ pypesto/petab/__init__.py
@@ -7,15 +7,18 @@ pyPESTO support for the PEtab data format.
 from .importer import PetabImporter
 
 # PEtab and amici are optional dependencies
+
+import warnings
+
 try:
     import petab
 except ImportError:
-    raise ImportError("PEtab import requires an installation of petab "
-                      "(https://github.com/PEtab-dev/PEtab). "
-                      "Install via `pip3 install petab`.")
+    warnings.warn("PEtab import requires an installation of petab "
+                  "(https://github.com/PEtab-dev/PEtab). "
+                  "Install via `pip3 install petab`.")
 try:
     import amici
 except ImportError:
-    raise ImportError("PEtab import requires an installation of amici "
-                      "(https://github.com/AMICI-dev/AMICI). "
-                      "Install via `pip3 install amici`.")
+    warnings.warn("PEtab import requires an installation of amici "
+                  "(https://github.com/AMICI-dev/AMICI). "
+                  "Install via `pip3 install amici`.")
