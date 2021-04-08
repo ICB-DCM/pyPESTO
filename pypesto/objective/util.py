Fix factor 2 in sres2fim, fixes #618

pypesto/objective/util.py
@@ -31,4 +31,4 @@ def sres_to_fim(sres: np.ndarray):
     """
     if sres is None:
         return None
-    return sres.transpose().dot(sres)
+    return 2 * sres.transpose().dot(sres)