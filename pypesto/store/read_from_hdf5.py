e1111c5d1e4e54a932a8abd9becfc9876af2187f
PaulJonasJost <jost.pauljonas@gmail.com>
2021-04-24 14:15:28 +0200

    integrated suggestions.

--- pypesto/store/read_from_hdf5.py
+++ pypesto/store/read_from_hdf5.py
@@ -248,17 +248,16 @@ class ProfileResultHDF5Reader:
                 self.results.problem = problem_reader.read()
             for profile_id in f['/profiling']:
                 profiling_list.append([
-                    None for _ in range(len(f[f'/profiling/{profile_id}']))
+                    None for _ in f[f'/profiling/{profile_id}']
                 ])
                 for parameter_id in f[f'/profiling/{profile_id}']:
                     if f[f'/profiling/{profile_id}/'
                          f'{parameter_id}'].attrs['IsNone']:
                         continue
-                    else:
-                        profiling_list[int(profile_id)][int(parameter_id)] = \
-                            read_hdf5_profile(f,
-                                              profile_id=profile_id,
-                                              parameter_id=parameter_id)
+                    profiling_list[int(profile_id)][int(parameter_id)] = \
+                        read_hdf5_profile(f,
+                                          profile_id=profile_id,
+                                          parameter_id=parameter_id)
             self.results.profile_result.list = profiling_list
         return self.results
 
