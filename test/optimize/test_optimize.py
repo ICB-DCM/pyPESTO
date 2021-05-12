Fix factor 2 in sres2fim; Add fast biological test model (CR) (#619)
* Fix factor 2 in sres2fim, fixes #618

* change chi2 -> fval in history; implement res2chi2+fval methods consistently

* fix sphinx

* Add biological test case based on CR model (#661)

* add cr test case

* fix test

* rerun amici nb as it fails for some reason

* fixup docstrings

* review feedback

* add docstring

* do not use sres_to_fim in test

* document get_objective; fix flake8

* test sphinx 4.0.1 as issues seem resolved there

* weird timeout problems

Co-authored-by: Fabian Fröhlich <fabian_froehlich@hms.harvard.edu>
Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
Co-authored-by: Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>

test/optimize/test_optimize.py
@@ -17,13 +17,22 @@ import pypesto
 import pypesto.optimize as optimize
 from pypesto.store import OptimizationResultHDF5Reader
 
-from ..util import rosen_for_sensi
+from ..util import rosen_for_sensi, CRProblem
 from numpy.testing import assert_almost_equal
 
 
-@pytest.fixture(params=['separated', 'integrated'])
-def mode(request):
-    return request.param
+@pytest.fixture(params=['cr', 'rosen-integrated', 'rosen-separated'])
+def problem(request) -> pypesto.Problem:
+    if request.param == 'cr':
+        return CRProblem().get_problem()
+    elif 'rosen' in request.param:
+        integrated = 'integrated' in request.param
+        obj = rosen_for_sensi(max_sensi_order=2, integrated=integrated)['obj']
+        lb = 0 * np.ones((1, 2))
+        ub = 1 * np.ones((1, 2))
+        return pypesto.Problem(objective=obj, lb=lb, ub=ub)
+    else:
+        raise ValueError("Unexpected input")
 
 
 optimizers = [
@@ -65,18 +74,13 @@ def optimizer(request):
     return request.param
 
 
-def test_optimization(mode, optimizer):
+def test_optimization(problem, optimizer):
     """Test optimization using various optimizers and objective modes."""
-    if mode == 'separated':
-        obj = rosen_for_sensi(max_sensi_order=2, integrated=False)['obj']
-    else:  # mode == 'integrated':
-        obj = rosen_for_sensi(max_sensi_order=2, integrated=True)['obj']
-
     library, method = optimizer
 
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        check_minimize(obj, library, method)
+        check_minimize(problem, library, method)
 
 
 def test_unbounded_minimize(optimizer):
@@ -173,14 +177,10 @@ def get_optimizer(library, solver):
     return optimizer
 
 
-def check_minimize(objective, library, solver, allow_failed_starts=False):
+def check_minimize(problem, library, solver, allow_failed_starts=False):
     """Runs a single run of optimization according to the provided inputs
     and checks whether optimization yielded a solution."""
     optimizer = get_optimizer(library, solver)
-    lb = 0 * np.ones((1, 2))
-    ub = 1 * np.ones((1, 2))
-    problem = pypesto.Problem(objective, lb, ub)
-
     optimize_options = optimize.OptimizeOptions(
         allow_failed_starts=allow_failed_starts
     )