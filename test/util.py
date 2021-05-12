070a32c48644eec3c2300de078878e88ae769379
Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>
2021-05-12 11:29:41 +0200

    Add biological test case based on CR model (#661)
    * add cr test case
    
    * fix test

--- test/util.py
+++ test/util.py
@@ -6,6 +6,8 @@ import sys
 import numpy as np
 import scipy.optimize as so
 import importlib
+import autograd.numpy as anp
+from autograd import jacobian
 import pypesto
 
 try:
@@ -115,6 +117,105 @@ def poly_for_sensi(max_sensi_order, integrated=False, x=0.):
 
     return obj_for_sensi(fun, grad, hess,
                          max_sensi_order, integrated, x)
+
+
+class CRProblem:
+    """ODE model of a conversion reaction A <-> B.
+
+    Uses automatic differentiation for derivative calculation.
+    """
+
+    def __init__(
+        self, n_t: int = 10, max_t: float = 15., x0: anp.ndarray = None,
+        p_true: anp.ndarray = None, sigma: float = 0.02,
+        lb: anp.ndarray = None, ub: anp.ndarray = None,
+    ):
+        self.ts = anp.linspace(0, max_t, n_t)
+        if x0 is None:
+            x0 = anp.array([1., 0.])
+        self.x0: anp.ndarray = x0
+        if p_true is None:
+            p_true = anp.array([0.06, 0.08])
+        self.p_true: anp.ndarray = p_true
+        self.sigma: float = sigma
+        if lb is None:
+            lb = anp.array([0., 0.])
+        self.lb = lb
+        if ub is None:
+            ub = anp.array([0.5, 0.5])
+        self.ub = ub
+
+        y_true = self.get_fy()(self.p_true)
+        # seed random number generator for reproducibility
+        rng = anp.random.Generator(anp.random.PCG64(0))
+        self.data = y_true + sigma * rng.normal(size=y_true.shape)
+
+    def get_fy(self):
+        """Systems states, fully observed, analytic solution."""
+        def fy(p):
+            p0, p1 = p
+            e = anp.exp(- (p0 + p1) * self.ts)
+            x = 1 / (- p0 - p1) * anp.array([[- p1 - p0 * e, - p1 + p1 * e],
+                                             [- p0 + p0 * e, - p0 - p1 * e]])
+            y = anp.einsum("mnr,n->mr", x, self.x0)
+            return y
+        return fy
+
+    def get_fres(self):
+        """Residuals."""
+        def fres(p):
+            return ((self.get_fy()(p) - self.data) / self.sigma).flatten()
+        return fres
+
+    def get_fsres(self):
+        """Residual sensitivities"""
+        return jacobian(self.get_fres())
+
+    def get_ffim(self):
+        """Fisher information matrix."""
+        def ffim(p):
+            return pypesto.objective.util.sres_to_fim(self.get_fsres()(p))
+        return ffim
+
+    def get_fnllh(self):
+        """Negative log-likelihood (minimization function)."""
+        def fnllh(p):
+            return 0.5 * anp.sum(self.get_fres()(p)**2)
+        return fnllh
+
+    def get_fsnllh(self):
+        """Negative log-likelihood gradient."""
+        return jacobian(self.get_fnllh())
+
+    def get_fs2nllh(self):
+        """Negative log-likelihood Hessian."""
+        return jacobian(self.get_fsnllh())
+
+    def get_objective(
+        self, fun: bool = True, res: bool = True, max_sensi_order: int = 2,
+        fim_for_hess: bool = False,
+    ):
+        """Full pyPESTO objective function."""
+        if fim_for_hess:
+            fhess = self.get_ffim()
+        else:
+            fhess = self.get_fs2nllh()
+
+        return pypesto.Objective(
+            fun=self.get_fnllh() if fun else None,
+            grad=self.get_fsnllh() if fun and max_sensi_order >= 1 else None,
+            hess=fhess if fun and max_sensi_order >= 2 else None,
+            res=self.get_fres() if res else None,
+            sres=self.get_fsres() if res and max_sensi_order >= 1 else None,
+        )
+
+    def get_problem(self):
+        """Full pypesto problem."""
+        return pypesto.Problem(
+            objective=self.get_objective(),
+            lb=self.lb,
+            ub=self.ub,
+        )
 
 
 def load_amici_objective(example_name):
