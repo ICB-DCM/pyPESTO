e4ff1316d0a4c5cf615462191260951b830cf06f
PaulJonasJost <jost.pauljonas@gmail.com>
2021-06-24 11:55:14 +0200

    Readded write_array to __init__

--- pypesto/store/__init__.py
+++ pypesto/store/__init__.py
@@ -11,3 +11,4 @@ from .save_to_hdf5 import (ProblemHDF5Writer, OptimizationResultHDF5Writer,
 from .read_from_hdf5 import (ProblemHDF5Reader, OptimizationResultHDF5Reader,
                              ProfileResultHDF5Reader, SamplingResultHDF5Reader,
                              read_result, load_objective_config)
+from .hdf5 import write_array
