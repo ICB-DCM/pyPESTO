7f696e37277ab3c00bfe6dd38286cc4664685c03
Fabian Fröhlich <fabian_froehlich@hms.harvard.edu>
2021-10-09 00:52:52 +0200

    update for fides 0.6.0 (#733)
    * update for fides 0.6.0
    
    * increase min fides ver
    
    * extend fides tests
    
    * fixup
    
    * fix resfun
    
    * reduce fides testcases
    
    * reduce fides testcases
    
    * fixups and require fides 0.6.1
    
    * update docstring

--- test/optimize/test_optimize.py
+++ test/optimize/test_optimize.py
@@ -63,8 +63,11 @@ optimizers = [
         nlopt.GN_DIRECT_L_RAND_NOSCAL, nlopt.AUGLAG, nlopt.AUGLAG_EQ
     ]],
     *[('fides', solver) for solver in itt.product(
-        [None, fides.SR1(), fides.BFGS(), fides.DFP()],
-        [fides.SubSpaceDim.FULL, fides.SubSpaceDim.TWO]
+        [None, fides.BFGS(), fides.SR1(), fides.PSB(), fides.BB(), fides.BG(),
+         fides.Broyden(0.5), fides.SSM(), fides.TSSM(), fides.HybridFixed(),
+         fides.FX(), fides.GNSBFGS()],
+        [fides.SubSpaceDim.TWO, fides.SubSpaceDim.FULL,
+         fides.SubSpaceDim.STEIHAUG]
     )]
 ]
 
