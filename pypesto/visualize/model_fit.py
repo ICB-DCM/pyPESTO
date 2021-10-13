855a21b9c3a26acd888385723a60c99bc13174b4
PaulJonasJost <jost.pauljonas@gmail.com>
2021-10-13 10:46:04 +0200

    added function to plot trajectory of a model.

--- pypesto/visualize/model_fit.py
+++ pypesto/visualize/model_fit.py
@@ -3,12 +3,16 @@ Visualization of the model fit after optimization.
 
 Currently only for PEtab problems.
 """
-import petab
-from amici.petab_simulate import simulate_petab
+import numpy as np
+import matplotlib.pyplot as plt
 import amici.petab_import as petab_import
+import petab
+from ..problem import Problem
+from amici.petab_simulate import simulate_petab
 from petab.visualize import plot_problem
 from typing import Union, Sequence
 from ..result import Result
+from amici.plotting import plotObservableTrajectories, plotStateTrajectories
 from amici.petab_objective import rdatas_to_simulation_df
 
 
@@ -36,13 +40,18 @@ def visualize_optimized_model_fit(petab_problem: petab.Problem,
     ax: Axis object of the created plot.
     None: In case subplots are saved to file
     """
+    if petab_problem is not None:
+        if petab is None:
+            raise
+
     problem_parameters = \
         dict(zip(petab_problem.parameter_df.index,
                  result.optimize_result.list[0]['x']))
 
     amici_model = petab_import.import_petab_problem(
         petab_problem,
-        model_output_dir=kwargs.pop('model_output_dir', None))
+        model_output_dir=kwargs.pop('model_output_dir', None),
+        force_compile=kwargs.pop('force_compile', False))
 
     res = simulate_petab(petab_problem, amici_model=amici_model,
                          scaled_parameters=True,
@@ -58,4 +67,82 @@ def visualize_optimized_model_fit(petab_problem: petab.Problem,
                       simulations_df=sim_df,
                       **kwargs
                       )
+    return ax
+
+
+def time_trajectory_model(
+        result: Union[Result, Sequence[Result]],
+        problem: Problem = None,
+        # TODO: conditions: Union[str, Sequence[str]] = None,
+        timepoints: Union[np.ndarray, Sequence[np.ndarray]] = None,
+        n_timepoints: int = 1000,
+        state_ids: Union[str, Sequence[str]] = None,
+        observable_ids: Union[str, Sequence[str]] = None,):
+    """
+    Visualize the time trajectory of the model with given timepoints.
+    It does this by calling the amici plotting routines.
+
+    Parameters
+    ----------
+    result:
+        The result object from optimization.
+    problem:
+        A pypesto problem. Default is 'None' in which case result.problem is
+        used. Needed in case the result is loaded from hdf5.
+    timepoints:
+        Array of timepoints, at which the trajectory will be plotted.
+    n_timepoints:
+        Number of timepoints to be plotted between 0 and last measurement of
+        the model. Only used when timepoints==None.
+    state_ids:
+        Ids of the states to be plotted.
+    observable_ids:
+        Ids of the observable Ids to be plotted.
+
+    Returns
+    -------
+    ax: Axis object of the created plot.
+    """
+
+    if problem is None:
+        problem = result.problem
+    # add timepoints as needed
+    if timepoints is None:
+        end_time = max(problem.objective.edatas[0].getTimepoints())
+        timepoints = np.linspace(start=0,
+                                 stop=end_time,
+                                 num=n_timepoints)
+    obj = problem.objective.set_custom_timepoints(
+        timepoints_global=timepoints)
+
+    # evaluate objective with return dic = True to get data
+    parameters = result.optimize_result.list[0]['x']
+    ret = obj(parameters, mode='mode_fun',
+              sensi_orders=(0,), return_dict=True)
+
+    # if state_ or observable_id's is not None, get indices for these
+    state_indices = None
+    observable_indices = None
+    if state_ids is not None:
+        state_indices = [
+            problem.objective.amici_model.getStateIds().index(state_id)
+            for state_id in state_ids]
+    if observable_ids is not None:
+        observable_indices = [
+            problem.objective.amici_model.getObservableIds().index(obs_id)
+            for obs_id in observable_ids]
+
+    fig, ax = plt.subplots(len(ret['rdatas']), 2)
+    # enforce two dimensions in case there is only one condition
+    ax = np.atleast_2d(ax)
+
+    for i_cond, rdata in enumerate(ret['rdatas']):
+        plotStateTrajectories(rdata=rdata,
+                              state_indices=state_indices,
+                              ax=ax[i_cond, 0],
+                              model=problem.objective.amici_model)
+        plotObservableTrajectories(rdata=rdata,
+                                   observable_indices=observable_indices,
+                                   ax=ax[i_cond, 1],
+                                   model=problem.objective.amici_model)
     return ax
