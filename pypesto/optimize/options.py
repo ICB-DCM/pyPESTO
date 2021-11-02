0d0529b0fae03130afeece2eb7711a28df3ef8b4
FFroehlich <fabian@schaluck.com>
2021-11-02 18:00:38 -0400

    add option to disable hess/sres reporting

--- pypesto/optimize/options.py
+++ pypesto/optimize/options.py
@@ -13,15 +13,27 @@ class OptimizeOptions(dict):
     allow_failed_starts:
         Flag indicating whether we tolerate that exceptions are thrown during
         the minimization process.
+    report_sres:
+        Flag indicating whether sres will be stored in the results object.
+        Deactivating this option will improve memory consumption for large
+        scale problems.
+    report_hess:
+        Flag indicating whether hess will be stored in the results object.
+        Deactivating this option will improve memory consumption for large
+        scale problems.
     """
 
     def __init__(self,
                  startpoint_resample: bool = False,
-                 allow_failed_starts: bool = True):
+                 allow_failed_starts: bool = True,
+                 report_sres: bool = True,
+                 report_hess: bool = True):
         super().__init__()
 
         self.startpoint_resample: bool = startpoint_resample
         self.allow_failed_starts: bool = allow_failed_starts
+        self.report_sres: bool = report_sres
+        self.report_hess: bool = report_hess
 
     def __getattr__(self, key):
         try:
