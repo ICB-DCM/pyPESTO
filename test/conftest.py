23b27a32b40346e73aae60b7ca9432a99fcf58f0
yannikschaelte <yannik.schaelte@gmail.com>
2021-12-06 22:15:54 +0100

    simplify hdf5 file fixture

--- test/conftest.py
+++ test/conftest.py
@@ -3,16 +3,10 @@ import tempfile
 
 import pytest
 
-# whether to remove storage files after a test has finished
-REMOVE_STORE = True
-
 
 @pytest.fixture
 def hdf5_file():
     """Generate a temporary hdf5 file."""
-    store_file = tempfile.mkstemp(suffix=".hdf5")[1]
-    try:
-        yield store_file
-    finally:
-        if REMOVE_STORE and os.path.exists(store_file):
-            os.remove(store_file)
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        file = os.path.join(tmp_dir, "file.hdf5")
+        yield file
