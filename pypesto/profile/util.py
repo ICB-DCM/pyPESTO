fix profile missing gradient not correctly handled

pypesto/profile/util.py
@@ -81,11 +81,11 @@ def calculate_approximate_ci(
 
 
 def initialize_profile(
-        problem: Problem,
-        result: Result,
-        result_index: int,
-        profile_index: Iterable[int],
-        profile_list: int
+    problem: Problem,
+    result: Result,
+    result_index: int,
+    profile_index: Iterable[int],
+    profile_list: int,
 ) -> float:
     """
     Initialize profiling based on a previous optimization.
@@ -136,7 +136,8 @@ def initialize_profile(
         profile_index=profile_index,
         profile_list=profile_list,
         problem_dimension=problem.dim_full,
-        global_opt=global_opt)
+        global_opt=global_opt,
+    )
 
     # return the log-posterior of the global optimum (needed in order to
     # compute the log-posterior-ratio)
@@ -144,15 +145,14 @@ def initialize_profile(
 
 
 def fill_profile_list(
-        profile_result: ProfileResult,
-        optimizer_result: Dict[str, Any],
-        profile_index: Iterable[int],
-        profile_list: int,
-        problem_dimension: int,
-        global_opt: float
+    profile_result: ProfileResult,
+    optimizer_result: Dict[str, Any],
+    profile_index: Iterable[int],
+    profile_list: int,
+    problem_dimension: int,
+    global_opt: float,
 ) -> None:
-    """
-    Fill a ProfileResult.
+    """Fill a ProfileResult.
 
     Helper function for `initialize_profile`.
 
@@ -178,7 +178,7 @@ def fill_profile_list(
     if optimizer_result[GRAD] is not None:
         gradnorm = np.linalg.norm(optimizer_result[GRAD])
     else:
-        gradnorm = None
+        gradnorm = np.nan
 
     # create blank profile
     new_profile = ProfilerResult(
@@ -192,7 +192,8 @@ def fill_profile_list(
         n_fval=0,
         n_grad=0,
         n_hess=0,
-        message=None)
+        message=None,
+    )
 
     if profile_list is None:
         # All profiles have to be created from scratch

pypesto/profile/walk_along_profile.py
@@ -12,14 +12,14 @@ logger = logging.getLogger(__name__)
 
 
 def walk_along_profile(
-        current_profile: ProfilerResult,
-        problem: Problem,
-        par_direction: int,
-        optimizer: Optimizer,
-        options: ProfileOptions,
-        create_next_guess: Callable,
-        global_opt: float,
-        i_par: int
+    current_profile: ProfilerResult,
+    problem: Problem,
+    par_direction: int,
+    optimizer: Optimizer,
+    options: ProfileOptions,
+    create_next_guess: Callable,
+    global_opt: float,
+    i_par: int,
 ) -> ProfilerResult:
     """
     Compute half a profile.
@@ -94,17 +94,28 @@ def walk_along_profile(
             )
         else:
             # if too many parameters are fixed, there is nothing to do ...
-            fval = problem.objective([])
+            fval = problem.objective(np.array([]))
             optimizer_result = OptimizerResult(
-                id='0', x=np.array([]), fval=fval, n_fval=0, n_grad=0, n_res=0,
-                n_hess=0, n_sres=0, x0=np.array([]), fval0=fval, time=0)
+                id='0',
+                x=np.array([]),
+                fval=fval,
+                n_fval=0,
+                n_grad=0,
+                n_res=0,
+                n_hess=0,
+                n_sres=0,
+                x0=np.array([]),
+                fval0=fval,
+                time=0,
+            )
             optimizer_result.update_to_full(problem=problem)
 
         if optimizer_result[GRAD] is not None:
-            gradnorm = np.linalg.norm(optimizer_result[GRAD][
-                                      problem.x_free_indices])
+            gradnorm = np.linalg.norm(
+                optimizer_result[GRAD][problem.x_free_indices]
+            )
         else:
-            gradnorm = None
+            gradnorm = np.nan
 
         current_profile.append_profile_point(
             x=optimizer_result.x,
@@ -115,7 +126,8 @@ def walk_along_profile(
             exitflag=optimizer_result.exitflag,
             n_fval=optimizer_result.n_fval,
             n_grad=optimizer_result.n_grad,
-            n_hess=optimizer_result.n_hess)
+            n_hess=optimizer_result.n_hess,
+        )
 
     # free the profiling parameter again
     problem.unfix_parameters(i_par)