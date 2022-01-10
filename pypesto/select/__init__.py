3942a55d623d0f937af4bf71c6837d8533c21632
dilpath <dilan.private+github@outlook.com>
2022-01-10 21:40:38 +0100

    fix autodoc

--- pypesto/select/__init__.py
+++ pypesto/select/__init__.py
@@ -7,3 +7,13 @@ Perform model selection with a PEtab Select problem.
 
 from .misc import model_to_pypesto_problem
 from .problem import Problem
+
+try:
+    import petab_select
+except ImportError:
+    import warnings
+    warnings.warn(
+        "pyPESTO's model selection methods require an installation of PEtab "
+        "Select (https://github.com/PEtab-dev/petab_select). Install via "
+        "`pip3 install petab-select` or `pip3 install pypesto[select]`."
+    )
