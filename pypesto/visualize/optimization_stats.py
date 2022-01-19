1c144d4f8f841f4d0fd0c9a959efdc8c8c32e557
Jakob Vanhoefer <jakob.vanhoefer@uni-bonn.de>
2022-01-19 16:51:10 +0100

    resolve depreciation warning

--- pypesto/visualize/optimization_stats.py
+++ pypesto/visualize/optimization_stats.py
@@ -373,7 +373,7 @@ def stats_lowlevel(
         The plot axes.
     """
     fvals = result.optimize_result.fval
-    values = result.optimize_result.get_for_key(property_name)
+    values = [res[property_name] for res in result.optimize_result.list]
     values, fvals = delete_nan_inf(fvals, values)
 
     if start_indices is not None:
