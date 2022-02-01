a8b16b4f44c5b38dd30028b14633dc08600baf93
PaulJonasJost <jost.pauljonas@gmail.com>
2022-02-01 18:23:00 +0100

    changed __repr__ to summary function.

--- pypesto/result/optimize.py
+++ pypesto/result/optimize.py
@@ -120,7 +120,8 @@ class OptimizerResult(dict):
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
-    def __repr__(self):
+    def summary(self):
+        """Get summary of the object."""
         message = (
             "### Optimizer Result \n\n"
             f"* optimizer used: {self.optimizer} \n"
@@ -180,20 +181,20 @@ class OptimizeResult:
     def __len__(self):
         return len(self.list)
 
-    def __repr__(self):
+    def summary(self):
+        """Get summary of the object."""
         # perform clustering for better information
         clust, clustsize = assign_clusters(delete_nan_inf(self.fval)[1])
 
         message = (
             "## Optimization Result \n\n"
             f"* number of starts: {len(self)} \n"
-            f"* number of sucessfull starts: \n"
             f"* best value found {clustsize[0]} time(s) \n"
             f"* number of plateaus found: "
             f"{1 + max(clust) - sum(clustsize == 1)} \n"
             f"* best value: {self[0]['fval']}, "
             f"worst value: {self[-1]['fval']} \n\n"
-            f"A summary of the best run:\n\n{self[0]}"
+            f"A summary of the best run:\n\n{self[0].summary()}"
         )
 
         # TODO: n_succesfull, circular import for assign clusters
