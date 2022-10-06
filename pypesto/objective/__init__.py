e42a32e21e36aa12a532fcdb46bea5f879fc7102
Yannik Schälte <yannik.schaelte@gmail.com>
2022-10-06 17:18:20 +0100

    undo julia import

--- pypesto/objective/__init__.py
+++ pypesto/objective/__init__.py
@@ -8,7 +8,6 @@ from .amici import AmiciCalculator, AmiciObjectBuilder, AmiciObjective
 from .base import ObjectiveBase
 from .finite_difference import FD, FDDelta
 from .function import Objective
-from .julia import JuliaObjective
 from .priors import (
     NegLogParameterPriors,
     NegLogPriors,
