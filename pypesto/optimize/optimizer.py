899b2c29ab3c2047f781a48907554ac6c61fc803
Yannik Schälte <yannik.schaelte@gmail.com>
2022-10-11 07:11:36 +0100

    fix scipy maxiter deprecation warning

--- pypesto/optimize/optimizer.py
+++ pypesto/optimize/optimizer.py
@@ -440,7 +440,7 @@ class ScipyOptimizer(Optimizer):
         if self.is_least_squares():
             options = {'max_nfev': 1000, 'disp': False}
         else:
-            options = {'maxiter': 1000, 'disp': False}
+            options = {'maxfun': 1000, 'disp': False}
         return options
 
 
