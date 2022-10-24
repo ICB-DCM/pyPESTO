43ce635eb523d7a99c8f8d6db3c66fda15e425f0
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-10-24 13:43:32 +0200

    Remove hardcoded paths

--- test/test_hierarchical.py
+++ test/test_hierarchical.py
@@ -1,28 +1,38 @@
-import pypesto
-import pypesto.logging
 import logging
-import amici
 import time
 
-from pypesto.hierarchical.solver import AnalyticalInnerSolver, NumericalInnerSolver
+import amici
+from benchmark_models_petab import get_problem
 
-#pypesto.logging.log_to_console(level=logging.DEBUG)
+import pypesto
+import pypesto.logging
+from pypesto.hierarchical.solver import (
+    AnalyticalInnerSolver,
+    NumericalInnerSolver,
+)
+from pypesto.petab import PetabImporter
+
+# pypesto.logging.log_to_console(level=logging.DEBUG)
 
 
 def test_sth():
-    yaml_file = "/home/yannik/benchmark-models-petab/Benchmark-Models/Boehm_JProteomeRes2014/Boehm_JProteomeRes2014.yaml"
-    #yaml_file = "/home/yannik/benchmark-models-petab/Benchmark-Models/Fujita_SciSignal2010/Fujita_SciSignal2010.yaml"
-    importer = pypesto.PetabImporter.from_yaml(yaml_file)
+    petab_problem = get_problem("Boehm_JProteomeRes2014")
+    # petab_problem = get_problem("Fujita_SciSignal2010")
+    importer = PetabImporter(petab_problem)
     objective = importer.create_objective(hierarchical=True)
     problem = importer.create_problem(objective)
-    problem.objective.amici_solver.setSensitivityMethod(amici.SensitivityMethod_adjoint)
+    problem.objective.amici_solver.setSensitivityMethod(
+        amici.SensitivityMethod_adjoint
+    )
     print(importer.petab_problem.x_nominal_free_scaled)
     ret = problem.objective(importer.petab_problem.x_nominal_free_scaled[:-6])
     print(ret)
 
     objective2 = importer.create_objective(hierarchical=False)
     problem2 = importer.create_problem(objective2)
-    problem2.objective.amici_solver.setSensitivityMethod(amici.SensitivityMethod_adjoint)
+    problem2.objective.amici_solver.setSensitivityMethod(
+        amici.SensitivityMethod_adjoint
+    )
     ret = problem2.objective(importer.petab_problem.x_nominal_free_scaled)
     print(ret)
 
@@ -31,7 +41,9 @@ def test_sth():
     problem2.objective.amici_solver.setAbsoluteTolerance(1e-8)
     problem2.objective.amici_solver.setRelativeTolerance(1e-8)
     n_starts = 50
-    startpoints = pypesto.startpoint.latin_hypercube(n_starts=n_starts, lb=problem2.lb, ub=problem2.ub)
+    startpoints = pypesto.startpoint.latin_hypercube(
+        n_starts=n_starts, lb=problem2.lb, ub=problem2.ub
+    )
     problem.x_guesses = startpoints[:, :-6]
     print(problem.x_guesses)
     problem2.x_guesses = startpoints
@@ -55,3 +67,5 @@ def test_sth():
     # result = pypesto.minimize(problem2, n_starts=n_starts, engine=engine)
     # print(result.optimize_result.get_for_key('fval'))
     # print(time.time() - start_time)
+
+    # TODO assert something
