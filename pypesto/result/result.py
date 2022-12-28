5a5755d94aa62771df8d518dcc9cd5d3938e3c27
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2022-12-28 12:21:36 +0100

    pypesto.Result: allow passing results to `__init__`

--- pypesto/result/result.py
+++ pypesto/result/result.py
@@ -24,11 +24,17 @@ class Result:
         The results of the sampler run.
     """
 
-    def __init__(self, problem=None):
+    def __init__(
+        self,
+        problem=None,
+        optimize_result: OptimizeResult = None,
+        profile_result: ProfileResult = None,
+        sample_result: SampleResult = None,
+    ):
         self.problem = problem
-        self.optimize_result = OptimizeResult()
-        self.profile_result = ProfileResult()
-        self.sample_result = SampleResult()
+        self.optimize_result = optimize_result or OptimizeResult()
+        self.profile_result = profile_result or ProfileResult()
+        self.sample_result = sample_result or SampleResult()
 
     def summary(self) -> str:
         """Get summary of the object."""
