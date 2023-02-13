15df3e175d6d1b951b1297d5a36ad7e3345670b1
Doresic <domagoj.doresic@gmail.com>
2023-02-13 11:12:15 +0100

    Fix visualization from result

--- pypesto/hierarchical/spline_approximation/visualize.py
+++ pypesto/hierarchical/spline_approximation/visualize.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Dict, List
 
 import matplotlib.pyplot as plt
@@ -7,35 +8,109 @@ from ...result import Result
 from .problem import SplineInnerProblem
 from .solver import SplineInnerSolver, get_spline_mapped_simulations
 
+try:
+    import amici
+except ImportError:
+    pass
 
-def plot_from_pypesto_result(pypesto_result: Result, **kwargs):
-    """Plot the inner solutions from a pypesto result."""
 
+def plot_splines_from_pypesto_result(
+    pypesto_result: Result, start_index=0, **kwargs
+):
+    """Plot the inner solutions from a pypesto result.
+
+    Parameters
+    ----------
+    pypesto_result:
+        The pypesto result.
+    start_index:
+        The index of the pypesto_result.optimize_result.list to plot.
+    kwargs:
+        Additional arguments to pass to the plotting function.
+
+    Returns
+    -------
+    fig:
+        The figure.
+    ax:
+        The axes.
+    """
+
+    # Get the parameters from the pypesto result for the start_index.
+    x_dct = dict(
+        zip(
+            pypesto_result.problem.objective.x_ids,
+            pypesto_result.optimize_result.list[start_index]['x'],
+        )
+    )
+
+    # Get the needed objects from the pypesto problem.
+    edatas = pypesto_result.problem.objective.edatas
+    parameter_mapping = pypesto_result.problem.objective.parameter_mapping
+    amici_model = pypesto_result.problem.objective.amici_model
+    amici_solver = pypesto_result.problem.objective.amici_solver
+    n_threads = pypesto_result.problem.objective.n_threads
+
+    # Fill in the parameters.
+    amici.parameter_mapping.fill_in_parameters(
+        edatas=edatas,
+        problem_parameters=x_dct,
+        scaled_parameters=True,
+        parameter_mapping=parameter_mapping,
+        amici_model=amici_model,
+    )
+
+    # Simulate the model with the parameters from the pypesto result.
+    inner_rdatas = amici.runAmiciSimulations(
+        amici_model,
+        amici_solver,
+        edatas,
+        num_threads=min(n_threads, len(edatas)),
+    )
+
+    # If any amici simulation failed, raise warning and return None.
+    if any(rdata.status != amici.AMICI_SUCCESS for rdata in inner_rdatas):
+        warnings.warn(
+            'Warning: Some AMICI simulations failed. Cannot plot inner solutions.'
+        )
+        return None
+
+    # Get simulation and sigma.
+    sim = [rdata['y'] for rdata in inner_rdatas]
+    sigma = [rdata['sigmay'] for rdata in inner_rdatas]
+
+    # Get the inner solver and problem.
+    inner_solver = pypesto_result.problem.objective.calculator.inner_solver
     inner_problem = pypesto_result.problem.objective.calculator.inner_problem
 
-    groups = inner_problem.groups.keys()
+    inner_results = inner_solver.solve(inner_problem, sim, sigma)
 
-    # Get the inner results
-    results = []
-    for group in groups:
-        result = {}
-        parameter_values = [
-            x.value for x in inner_problem.get_xs_for_group(group)
-        ]
-        parameter_values.insert(0, 0)
-
-        # Get the reformulated parameters as the difference between the parameter_values
-        reformulated_parameter_values = np.diff(parameter_values)
-        result['x'] = reformulated_parameter_values
-        results.append(result)
-
-    return plot_from_inner_result(inner_problem, results, **kwargs)
+    return plot_splines_from_inner_result(
+        inner_problem, inner_results, **kwargs
+    )
 
 
-def plot_from_inner_result(
+def plot_splines_from_inner_result(
     inner_problem: SplineInnerProblem, results: List[Dict], **kwargs
 ):
-    """Plot the inner solutions."""
+    """Plot the inner solutions.
+
+    Parameters
+    ----------
+    inner_problem:
+        The inner problem.
+    results:
+        The results from the inner solver.
+    kwargs:
+        Additional arguments to pass to the plotting function.
+
+    Returns
+    -------
+    fig:
+        The figure.
+    ax:
+        The axes.
+    """
 
     if len(results) != len(inner_problem.groups):
         raise ValueError(
