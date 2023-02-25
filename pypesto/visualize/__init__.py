a606f9384780f834d590a220574f6d4b0610b0b9
PaulJonasJost <jost.pauljonas@gmail.com>
2023-02-25 12:34:28 +0100

    Added notebook for workshop

--- pypesto/visualize/__init__.py
+++ pypesto/visualize/__init__.py
@@ -30,6 +30,7 @@ from .parameters import (
     optimization_scatter,
     parameter_hist,
     parameters,
+    parameters_correlation_matrix,
     parameters_lowlevel,
 )
 from .profile_cis import profile_cis
