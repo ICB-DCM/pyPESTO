1631499c35f83c328969ea4b868ea7b842375098
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-06-05 14:33:10 +0200

    fix doc

--- pypesto/petab/importer.py
+++ pypesto/petab/importer.py
@@ -335,8 +335,9 @@ class PetabImporter(AmiciObjectBuilder):
 
         Parameters
         ----------
-        kwargs: Extra arguments passed to :meth:`amici.SbmlImporter.sbml2amici`
-            or:meth:`amici.pysb_import.pysb2amici`.
+        kwargs:
+            Extra arguments passed to :meth:`amici.SbmlImporter.sbml2amici`
+            or :meth:`amici.pysb_import.pysb2amici`.
         """
         # delete output directory
         if os.path.exists(self.output_folder):
