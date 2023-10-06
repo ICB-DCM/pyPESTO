ae6037960b5d7ed14d5cdaf33a477ee01de53e0a
PaulJonasJost <jost.pauljonas@gmail.com>
2023-10-06 11:30:18 -0400

    Added a bit more information to petab importer and objective function created from petab. Line with "noqa" to keep url. Closes #1047

--- pypesto/petab/importer.py
+++ pypesto/petab/importer.py
@@ -64,7 +64,11 @@ class PetabImporter(AmiciObjectBuilder):
     Importer for Petab files.
 
     Create an `amici.Model`, an `objective.AmiciObjective` or a
-    `pypesto.Problem` from Petab files.
+    `pypesto.Problem` from Petab files. The created objective function is a
+    negative log-likelihood function and can thus be negative. The actual
+    form of the likelihood depends on the noise model specified in petab.
+    For more information, see
+    [the documentation in petab](https://petab.readthedocs.io/en/latest/documentation_data_format.html#noise-distributions)  # noqa
     """
 
     MODEL_BASE_DIR = "amici_models"
