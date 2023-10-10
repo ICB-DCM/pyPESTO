beb8b9e6cf678273a8dc6fad04db22e5c8724bc8
PaulJonasJost <jost.pauljonas@gmail.com>
2023-10-10 10:01:13 -0400

    Adhere to new petabl.jl 2.0 version syntax

--- doc/example/conversion_reaction/PEtabJl_module.jl
+++ doc/example/conversion_reaction/PEtabJl_module.jl
@@ -5,15 +5,14 @@ using Sundials
 using PEtab
 
 pathYaml = "/Users/pauljonasjost/Documents/GitHub_Folders/pyPESTO/test/julia/../../doc/example/conversion_reaction/conversion_reaction.yaml"
-petabModel = readPEtabModel(pathYaml, verbose=true)
+petabModel = PEtabModel(pathYaml, verbose=true)
 
 # A full list of options for createPEtabODEProblem can be found at https://sebapersson.github.io/PEtab.jl/dev/API_choosen/#PEtab.setupPEtabODEProblem
-petabProblem = createPEtabODEProblem(
+petabProblem = PEtabODEProblem(
 	petabModel,
-	odeSolverOptions=ODESolverOptions(Rodas5P(), abstol=1e-08, reltol=1e-08, maxiters=Int64(1e4)),
-	gradientMethod=:ForwardDiff,
-	hessianMethod=:ForwardDiff,
-	sparseJacobian=nothing,
+	ode_solver=ODESolver(Rodas5P(), abstol=1e-08, reltol=1e-08, maxiters=Int64(1e4)),
+	gradient_method=:ForwardDiff,
+	hessian_method=:ForwardDiff,
 	verbose=true
 )
 
