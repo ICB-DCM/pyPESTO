b3073d41e624a612b41c458e834a7371786e407f
Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>
2023-10-13 08:38:31 -0400

    Update doc/example/conversion_reaction/PEtabJl_module.jl

--- doc/example/conversion_reaction/PEtabJl_module.jl
+++ doc/example/conversion_reaction/PEtabJl_module.jl
@@ -7,7 +7,7 @@ using PEtab
 pathYaml = "/Users/pauljonasjost/Documents/GitHub_Folders/pyPESTO/test/julia/../../doc/example/conversion_reaction/conversion_reaction.yaml"
 petabModel = PEtabModel(pathYaml, verbose=true)
 
-# A full list of options for createPEtabODEProblem can be found at https://sebapersson.github.io/PEtab.jl/dev/API_choosen/#PEtab.setupPEtabODEProblem
+# A full list of options for PEtabODEProblem can be found at https://sebapersson.github.io/PEtab.jl/stable/API_choosen/
 petabProblem = PEtabODEProblem(
 	petabModel,
 	ode_solver=ODESolver(Rodas5P(), abstol=1e-08, reltol=1e-08, maxiters=Int64(1e4)),
