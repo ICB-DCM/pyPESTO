19e99bfd754856949826414ef6846cfa957a3bd5
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-06-17 15:01:16 +0200

    ..

--- pypesto/objective/amici/amici.py
+++ pypesto/objective/amici/amici.py
@@ -488,7 +488,7 @@ class AmiciObjective(ObjectiveBase):
         #  based on the supplied model. If not, raise an error.
         #  This has to be done on every call, since the preprocessor may
         #  change the parameters w.r.t. which sensitivities are required.
-        if max(sensi_orders) > 0 and (
+        if max(0, *sensi_orders) > 0 and (
             missing_sensitivities := self._get_missing_sensitivities()
         ):
             raise ValueError(
