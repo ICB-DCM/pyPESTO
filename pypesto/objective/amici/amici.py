7ba3815a863140eabb4626509558717eed3cb773
Daniel Weindl <daniel.weindl@uni-bonn.de>
2024-11-19 11:35:08 +0100

    ..

--- pypesto/objective/amici/amici.py
+++ pypesto/objective/amici/amici.py
@@ -707,3 +707,9 @@ class AmiciObjective(ObjectiveBase):
             ) in condition_mapping.map_sim_var.items():
                 if (val := id_to_val.get(mapped_to_par)) is not None:
                     condition_mapping.map_sim_var[model_par] = val
+            for (
+                model_par,
+                mapped_to_par,
+            ) in condition_mapping.map_sim_fix.items():
+                if (val := id_to_val.get(mapped_to_par)) is not None:
+                    condition_mapping.map_sim_fix[model_par] = val
