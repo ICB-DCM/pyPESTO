99100d059d6aa50210c1215e5adf13b4307d1d75
PaulJonasJost <jost.pauljonas@gmail.com>
2025-03-13 10:34:59 +0100

    Added more examples

---
+++ pypesto/examples/example_class.py
@@ -0,0 +1,144 @@
+"""Define a simple example class for pypesto examples."""
+import os
+import tempfile
+from abc import ABC, abstractmethod
+
+import petab.v1 as petab
+import requests
+
+from ..objective import ObjectiveBase
+from ..petab import PetabImporter
+from ..problem import Problem
+
+
+class PyPESTOExampleBase(ABC):
+    """Abstract example class for pypesto examples.
+
+    Examples provide a simple wrapper around generic and in documentation
+    used examples. They provide a simple interface to load the objective and
+    problem functions.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        detailed_description: str | None = None,
+    ):
+        """
+        Initialize the example.
+
+        Parameters
+        ----------
+        name:
+            The name of the example.
+        description:
+            A short description of the example.
+        detailed_description:
+            A detailed description of the example.
+        """
+        self.name = name
+        self.description = description
+        self.detailed_description = detailed_description
+
+    @property
+    @abstractmethod
+    def objective(self) -> ObjectiveBase:
+        """Returns the objective function of this example."""
+        pass
+
+    @property
+    @abstractmethod
+    def problem(self) -> Problem:
+        """Returns the problem of this example."""
+        pass
+
+
+class PyPESTOExamplePEtab(PyPESTOExampleBase):
+    """A PEtab example class for pypesto examples."""
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        github_repo: str,
+        filenames: list[str],
+        detailed_description: str | None = None,
+    ):
+        """
+        Initialize the example.
+
+        Parameters
+        ----------
+        name:
+            The name of the example.
+        description:
+            A short description of the example.
+        github_repo:
+            The github repository to download the example from.
+        filenames:
+            The filenames to download.
+        detailed_description:
+            A detailed description of the example.
+        """
+        super().__init__(name, description, detailed_description)
+        self.github_repo = github_repo
+        self.filenames = filenames
+        self.petab_yaml = next(
+            (filename for filename in filenames if filename.endswith(".yaml")),
+            None,
+        )
+
+        self._importer = None
+        self._petab_problem = None
+        self._objective = None
+        self._problem = None
+
+    @property
+    def petab_problem(self) -> petab.Problem:
+        """Load the PEtab problem."""
+        if self._petab_problem is not None:
+            return self._petab_problem
+        with tempfile.TemporaryDirectory() as temp_dir:
+            download_success = self.download_files(temp_dir)
+            if not download_success:
+                raise FileNotFoundError(
+                    f"Could not download files from {self.github_repo}. "
+                    f"Check your internet connection."
+                )
+            self._petab_problem = petab.Problem.from_yaml(
+                os.path.join(temp_dir, self.petab_yaml)
+            )
+        return self._petab_problem
+
+    @property
+    def importer(self) -> PetabImporter:
+        """Load the importer."""
+        if self._importer is None:
+            self._importer = PetabImporter(self.petab_problem)
+        return self._importer
+
+    @property
+    def objective(self) -> ObjectiveBase:
+        """Load the objective function."""
+        if self._objective is None:
+            self._objective = self.problem.objective
+        return self._objective
+
+    @property
+    def problem(self) -> Problem:
+        """Load the problem."""
+        if self._problem is None:
+            self._problem = self.importer.create_problem()
+        return self._problem
+
+    def download_files(self, dir: str):
+        """Download the petab files from the github repository to ``dir``."""
+        for filename in self.filenames:
+            url = f"{self.github_repo}/{filename}"
+            response = requests.get(url, timeout=10)
+            if response.status_code != 200:
+                return False
+            with open(os.path.join(dir, filename), "wb") as file:
+                file.write(response.content)
+        return True
---
+++ pypesto/examples/examples.py
@@ -0,0 +1,110 @@
+from .example_class import PyPESTOExamplePEtab
+
+boehm = PyPESTOExamplePEtab(
+    name="boehm_JProteomeRes2014",
+    description="A model of the STAT5 dimerization. Based on the publication "
+    "by Boehm et al. (2014) https://doi.org/10.1021/pr5006923",
+    github_repo="https://raw.githubusercontent.com/ICB-DCM/pyPESTO/refs/heads/"
+    "main/doc/example/boehm_JProteomeRes2014",
+    filenames=[
+        "boehm_JProteomeRes2014.yaml",
+        "parameters_Boehm_JProteomeRes2014.tsv",
+        "experimentalCondition_Boehm_JProteomeRes2014.tsv",
+        "measurementData_Boehm_JProteomeRes2014.tsv",
+        "observables_Boehm_JProteomeRes2014.tsv",
+        "boehm_JProteomeRes2014.xml",
+        "visualizationSpecification_Boehm_JProteomeRes2014.tsv",
+    ],
+)
+
+censored_data = PyPESTOExamplePEtab(
+    name="censored_data",
+    description="A model with censored data. Right censoring >20, left <3, "
+    "interval censoring [10, 16].",
+    github_repo="https://raw.githubusercontent.com/ICB-DCM/pyPESTO/refs/heads/"
+    "main/doc/example/example_censored",
+    filenames=[
+        "example_censored.yaml",
+        "parameters_example_censored.tsv",
+        "experimentalCondition_example_censored.tsv",
+        "measurementData_example_censored.tsv",
+        "observables_example_censored.tsv",
+        "model_example_censored.xml",
+    ],
+    detailed_description="This model was taken from "
+    "https://doi.org/10.1093/bioinformatics/btab512 and "
+    "censoring was added to it. The PEtab problem was "
+    "adjusted accordingly.",
+)
+
+ordinal_data = PyPESTOExamplePEtab(
+    name="ordinal_data",
+    description="A model with ordinal data. The data is binned into three "
+    "categories.",
+    github_repo="https://raw.githubusercontent.com/ICB-DCM/pyPESTO/refs/heads/"
+    "main/doc/example/example_ordinal",
+    filenames=[
+        "example_ordinal.yaml",
+        "parameters_example_ordinal.tsv",
+        "experimentalCondition_example_ordinal.tsv",
+        "measurementData_example_ordinal.tsv",
+        "observables_example_ordinal.tsv",
+        "model_example_ordinal.xml",
+    ],
+    detailed_description="This model was taken from "
+    "https://doi.org/10.1093/bioinformatics/btab512 and "
+    "The PEtab problem was slightly adjusted.",
+)
+
+semiquantitative_data = PyPESTOExamplePEtab(
+    name="semiquantitative_data",
+    description="A model with semiquantitative data. Nonlinear "
+    "transformations were applied to the data.",
+    github_repo="https://raw.githubusercontent.com/ICB-DCM/pyPESTO/refs/heads/"
+    "main/doc/example/example_semiquantitative",
+    filenames=[
+        "example_semiquantitative.yaml",
+        "parameters_example_semiquantitative.tsv",
+        "experimentalCondition_example_semiquantitative.tsv",
+        "measurementData_example_semiquantitative.tsv",
+        "observables_example_semiquantitative.tsv",
+        "model_example_semiquantitative.xml",
+    ],
+    detailed_description="This model was taken from "
+    "https://doi.org/10.1093/bioinformatics/btab512 and "
+    "the PEtab problem was adjusted.",
+)
+
+semiquantitative_data_linear = PyPESTOExamplePEtab(
+    name="semiquantitative_data_linear",
+    description="A model with semiquantitative data. Linear "
+    "transformations were applied to the data.",
+    github_repo="https://raw.githubusercontent.com/ICB-DCM/pyPESTO/refs/heads/"
+    "main/doc/example/example_semiquantitative",
+    filenames=[
+        "example_semiquantitative_linear.yaml",
+        "parameters_example_semiquantitative.tsv",
+        "experimentalCondition_example_semiquantitative.tsv",
+        "measurementData_example_semiquantitative_linear.tsv",
+        "observables_example_semiquantitative.tsv",
+        "model_example_semiquantitative.xml",
+    ],
+    detailed_description="This model was taken from "
+    "https://doi.org/10.1093/bioinformatics/btab512 and "
+    "the PEtab problem was adjusted.",
+)
+
+conversion_reaction = PyPESTOExamplePEtab(
+    name="conversion_reaction",
+    description="A simple conversion reaction model. A -> B and B -> A.",
+    github_repo="https://raw.githubusercontent.com/ICB-DCM/pyPESTO/refs/heads/"
+    "main/doc/example/conversion_reaction",
+    filenames=[
+        "conversion_reaction.yaml",
+        "parameters.tsv",
+        "conditions.tsv",
+        "measurements.tsv",
+        "observables.tsv",
+        "model_conversion_reaction.xml",
+    ],
+)
