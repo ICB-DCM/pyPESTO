7164cbd30b0ed4d0813ac35d036bdb7cffc7be7a
Jonas Arruda <69197639+arrjon@users.noreply.github.com>
2025-12-16 13:15:15 +0100

    Prior sample (#1622)
    * sampling from prior as start
    
    * sampling from prior as start
    
    * add logger
    
    * add logger
    
    * better initial starting sampling
    
    * radnom seed
    
    * fix PetabStartpoints
    
    * fix notebook
    
    * update docstring
    
    * fix startpoint bug
    
    * add warm start for PT
    
    * return nan if no prior given
    
    * warm start PT
    
    * warm start PT
    
    * return samples as dict
    
    * fix prior test
    
    * Update pypesto/problem/base.py
    
    Co-authored-by: Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>
    
    * update notebook
    
    * Update pypesto/objective/priors.py
    
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    
    * Update pypesto/objective/priors.py
    
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    
    * Update pypesto/objective/priors.py
    
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    
    * Update pypesto/startpoint/prior.py
    
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    
    * Update pypesto/startpoint/prior.py
    
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    
    * improved comments
    
    * improved comments
    
    * Combine _resample_out_of_bounds and sample
    
    * improve comments
    
    * fix scale in sampling
    
    * improved test
    
    * Apply suggestions from code review
    
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    
    * better type hints
    
    * improve comments
    
    * improve kwargs
    
    * fix test
    
    * Update pypesto/startpoint/prior.py
    
    Co-authored-by: Daniel Weindl <dweindl@users.noreply.github.com>
    
    * Update pypesto/objective/priors.py
    
    Co-authored-by: Daniel Weindl <dweindl@users.noreply.github.com>
    
    * Update pypesto/objective/priors.py
    
    Co-authored-by: Daniel Weindl <dweindl@users.noreply.github.com>
    
    * fix type hint
    
    * fix type hint
    
    ---------
    
    Co-authored-by: Paul Jonas Jost <70631928+PaulJonasJost@users.noreply.github.com>
    Co-authored-by: Dilan Pathirana <59329744+dilpath@users.noreply.github.com>
    Co-authored-by: Daniel Weindl <dweindl@users.noreply.github.com>

--- pypesto/sample/sample.py
+++ pypesto/sample/sample.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from ..problem import Problem
 from ..result import Result
+from ..startpoint import PriorStartpoints
 from ..store import autosave
 from .adaptive_metropolis import AdaptiveMetropolisSampler
 from .sampler import Sampler
@@ -20,6 +21,7 @@ def sample(
     sampler: Sampler = None,
     x0: Union[np.ndarray, list[np.ndarray]] = None,
     result: Result = None,
+    warm_start: float = 1.0,
     filename: Union[str, Callable, None] = None,
     overwrite: bool = False,
 ) -> Result:
@@ -41,6 +43,12 @@ def sample(
         found in optimization is used. Note that some samplers require an
         initial parameter, some may ignore it. x0 can also be a list,
         to have separate starting points for parallel tempering chains.
+    warm_start:
+        Whether to warm start from previous optimization results stored in
+        `result` or to sample from the prior. The value is the convex
+        combination between the two, with 1.0 meaning only warm start and
+        0.0 meaning only sample from prior. If `x0` is provided,
+        `warm_start` is ignored.
     result:
         A result to write to. If None provided, one is created from the
         problem.
@@ -67,14 +75,44 @@ def sample(
     if n_samples is not None:
         n_samples = bound_n_samples_from_env(n_samples)
 
+    if warm_start > 1.0 or warm_start < 0.0:
+        raise ValueError("warm_start must be in [0, 1].")
+
     # try to find initial parameters
     if x0 is None:
-        result.optimize_result.sort()
-        if len(result.optimize_result.list) > 0:
-            x0 = problem.get_reduced_vector(
-                result.optimize_result.list[0]["x"]
+        if warm_start == 0:
+            logger.info("Sampling initial points from prior.")
+            get_start_params = PriorStartpoints(check_fval=True)
+            x0 = get_start_params.sample(
+                n_starts=1,
+                lb=problem.lb,
+                ub=problem.ub,
+                priors=problem.x_priors,
+            )[0]
+        elif result.optimize_result is not None:
+            result.optimize_result.sort()
+            if len(result.optimize_result.list) > 0:
+                x0 = problem.get_reduced_vector(
+                    result.optimize_result.list[0]["x"]
+                )
+            if warm_start < 1.0 and x0 is not None:
+                logger.info(
+                    f"Initializing sampling with a warm start from optimization "
+                    f"and prior sampling with weight: {warm_start}."
+                )
+                get_start_params = PriorStartpoints(check_fval=True)
+                x0_prior = get_start_params.sample(
+                    n_starts=1,
+                    lb=problem.lb,
+                    ub=problem.ub,
+                    priors=problem.x_priors,
+                )[0]
+                x0 = warm_start * x0 + (1 - warm_start) * x0_prior
+        else:
+            logger.info(
+                "No initial point provided and no optimization result found. Set warm_start to 0 "
+                "if you need initial points for sampling."
             )
-        # TODO multiple x0 for PT, #269
 
     # set sampler
     if sampler is None:
