8aac10c1c14ef86b0f9bb1c7ce892d382e2833a1
Daniel Weindl <dweindl@users.noreply.github.com>
2026-03-04 16:01:29 +0100

    doc: More readable figures in examples (#1691)
    * Update figsize/fontsize.
    * Less verbose profiles
    * Fix typos
    * Fix type annotation
    * Fix cropped waterfall markers

--- pypesto/profile/profile_next_guess.py
+++ pypesto/profile/profile_next_guess.py
@@ -98,7 +98,7 @@ def next_guess(
             max_step_reduce_factor,
         )
 
-    logger.info(
+    logger.debug(
         f"Next guess for {problem.x_names[par_index]} in direction "
         f"{par_direction} is {next_initial_guess[par_index]:.4f}. Step size: "
         f"{next_initial_guess[par_index] - x[par_index]:.4f}."
